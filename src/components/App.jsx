98ed436100da539b404610f7754483efa876522b
Brendan Do <brendan.do@sherwin.com>
2018-09-11 15:45:59 -0400

    Adding styled components with an example button (which of course should/can be deleted)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Button from './Button/Button.jsx';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to SWPrism</h1>
+        <h1>Welcome to Prism</h1>
+        <Button>This is a normal button</Button>
+        <Button primary>This is a primary button</Button>
       </div>
     );
   }
