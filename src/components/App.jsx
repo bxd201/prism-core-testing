8dbefb7258ff27c08f5006fe657878b970337a1c
Brendan Do <brendan.do@sherwin.com>
2018-09-12 07:47:41 -0400

    Fixing structure error in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,16 @@ import Button from './Button/Button.jsx';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to Prism</h1>
-        <Button>This is a normal button</Button>
-        <Button primary>This is a primary button</Button>
-      </div>
-      <div className="app-wrapper">
-        <ColorPath></ColorPath>
-	</div>
-	
+      <React.Fragment>
+        <div>
+          <h1>Welcome to Prism</h1>
+          <Button>This is a normal button</Button>
+          <Button primary>This is a primary button</Button>
+        </div>
+        <div className="app-wrapper">
+          <ColorPath></ColorPath>
+        </div>
+      </React.Fragment>
     );
   }
 }
