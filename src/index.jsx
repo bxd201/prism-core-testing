e6b6518e955d3bba7d890bbf074b7b4dd44953f4
Brendan Do <brendan.do@sherwin.com>
2018-09-13 09:14:34 -0400

    Adding the react binder logic along with internationalization.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
 
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// list all top level react components here
+const APPS = {
+	App,
+};
+
+const renderAppInElement = (el) => {
+	if(el.className.indexOf('__react-bound') > -1) {
+		return;
+	}
+
+	const reactComponent = el.getAttribute('data-react-component');
+	// if no data attribute specifying the react component exists, let's get out.
+	// although if it doesn't have this data attribute, it shouldn't have a __react-root class...
+	if(!reactComponent) {
+		console.warn(el, ' does not have a data-react-component specified.');
+		return;
+	}
+
+	const App = APPS[reactComponent];
+
+	// if the component doesn't exist, let's get out too
+	if(!App) {
+		console.warn(`${reactComponent} does is not included. Please import this component into index.jsx.`);
+		return;
+	}
+
+	// get props from elements data attribute, like the post_id
+	const props = Object.assign({}, el.dataset);
+
+	// remove the component declaration in the data attributes
+	delete props.reactComponent;
+
+	ReactDOM.render(
+		<IntlProvider locale={navigator.language || navigator.languages[0] || 'en-US'}>
+			<App {...props} />
+		</IntlProvider>, el);
+
+	el.classList.add('__react-bound');
+};
+
+const bindReactToDOM = () => {
+	document.querySelectorAll('.__react-root').forEach(renderAppInElement);
+};
+
+bindReactToDOM();
