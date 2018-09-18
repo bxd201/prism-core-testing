3c1a2d8c6278ea27ef432ee775a768711e9a2934
Brendan Do <brendan.do@sherwin.com>
2018-09-18 11:13:53 -0400

    Decoupling TintableScene from the CSS

--- src/components/TintableScene/TintableScene.jsx
+++ src/components/TintableScene/TintableScene.jsx
@@ -1,36 +1,62 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const RoomWrapper = styled.div`
+  height: 100%;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+  max-width: 900px;
+  max-height: 600px;
+`
+const SceneWrapper = styled.div`
+  height: 100%;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+  background-color: ${props => props.color ? props.color.cssrgb : ''};
+`
+const BaseScene = styled.img`
+  height: 100%;
+  left: 0;
+  overflow: hidden;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+`
+const TintedScene = styled(BaseScene)`
+  object-fit: cover;
+  filter: grayscale(100);
+  mix-blend-mode: multiply; 
+`
+const NaturalTintedScene = styled(BaseScene)`
+  object-fit: cover;
+  -webkit-mask-image: url( '/src/images/room-mask.png' );
+  -webkit-mask-size: cover;
+  -webkit-mask-position: 50% 50%;
+  mask-size: cover;
+  transition: opacity 0.9s;
+  opacity: 1;
+`
 
 class TintableScene extends PureComponent {
-  constructor (props) {
-    super(props)
-
-    this.tintColor = 'hsl( 53.571428571428584, 38.35616438356164%, 28.627450980392155% )'
-    this.secondaryTintColor = 'hsl(41.24999999999996, 34.782608695652165%, 90.98039215686275%)'
-    this.fixtureHueRotation = 33
-  }
-
   render () {
-    let tintColor = this.tintColor
-    if (this.props.color) {
-      const { color } = this.props
-      tintColor = color.cssrgb
-    }
-
     return (
-      <div className={'scene__room-wrapper scene__room-wrapper--bottom'} style={{ maxWidth: '900px', maxHeight: '600px' }}>
-        <div className='scene__room__tinted-wrapper' style={{ backgroundColor: tintColor }}>
-          <img className='scene__room scene__room--tinted' src='/src/images/room.jpg' alt='' />
-        </div>
-        <img className='scene__room scene__room--natural' src='/src/images/room.jpg' alt='' />
-        <div className='scene__room__tinted-wrapper scene__room__tinted-wrapper--rear-wall' style={{ backgroundColor: this.secondaryTintColor }}>
-          <img className='scene__room scene__room--tinted scene__room--rear-wall' src='/src/images/room.jpg' alt='' />
-        </div>
-        <div className='scene__room__tinted-wrapper scene__room__tinted-wrapper--fixtures' style={{ backgroundColor: this.secondaryTintColor }}>
-          <img className='scene__room scene__room--hue-rotated' src='/src/images/room.jpg' alt='' style={{ filter: `hue-rotate(${this.fixtureHueRotation}deg)` }} />
-        </div>
-      </div>
+      <RoomWrapper>
+        <SceneWrapper color={this.props.color}>
+          <TintedScene src='/src/images/room.jpg' alt='' />
+        </SceneWrapper>
+        <NaturalTintedScene src='/src/images/room.jpg' alt='' />
+      </RoomWrapper>
     )
   }
 }
