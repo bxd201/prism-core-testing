4d76c80a8657aea4cd925610b95ddf6d87bbd0d0
Brandon Chartier <brandon.chartier@sherwin.com>
2018-10-03 10:07:24 -0400

    Adding basic jest test scaffolding

---
+++ test/reducers.test.js
@@ -0,0 +1,42 @@
+import { colors } from '../src/reducers/colors'
+import { scenes } from '../src/reducers/scenes'
+
+test('colors reducer changes status', () => {
+  const state = colors({ status: 'pending' }, { type: 'REQUEST_COLORS', payload: 'done' })
+  expect(state).toEqual({ status: 'done' })
+})
+
+test('colors reducer updates items', () => {
+  const state = colors({ items: [] }, { type: 'RECEIVE_COLORS', payload: { colors: [{ test: true }] } })
+  expect(state).toEqual({ items: [{ test: true }] })
+})
+
+test('colors reducer default', () => {
+  const state = colors({ items: [] }, { type: 'TEST', payload: { colors: [{ test: true }] } })
+  expect(state).toEqual({ items: [] })
+})
+
+test('colors reducer undefined state', () => {
+  const state = colors(undefined, { type: 'TEST' })
+  expect(state).toEqual({})
+})
+
+test('scenes reducer changes selectedColor', () => {
+  const state = scenes({ selectedColor: 'blue' }, { type: 'COLOR_SELECTED', payload: { color: 'purple' } })
+  expect(state).toEqual({ selectedColor: 'purple' })
+})
+
+test('scenes reducer changes scene', () => {
+  const state = scenes({ scene: 'test' }, { type: 'SELECT_SCENE', payload: { scene: 'testing' } })
+  expect(state).toEqual({ scene: 'testing' })
+})
+
+test('scenes reducer default', () => {
+  const state = scenes({ scene: 'test' }, { type: 'TEST', payload: { scene: 'testing' } })
+  expect(state).toEqual({ scene: 'test' })
+})
+
+test('scenes reducer undefined state', () => {
+  const state = scenes(undefined, { type: 'TEST' })
+  expect(state).toEqual({})
+})
