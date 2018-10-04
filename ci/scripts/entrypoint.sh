a73157474d1c72085758f49b67737f1276888b9f
Jonathan Gnagy <jonathan.l.gnagy@sherwin.com>
2018-10-04 08:53:27 -0700

    Fixing env substitution

--- ci/scripts/entrypoint.sh
+++ ci/scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-envsubst < /usr/share/nginx/html/bundle.js.template > /usr/share/nginx/html/bundle.js
-envsubst < /usr/share/nginx/html/bundle.js.map.template > /usr/share/nginx/html/bundle.js.map
+envsubst '$API_URL' < /usr/share/nginx/html/bundle.js.template > /usr/share/nginx/html/bundle.js
+envsubst '$API_URL' < /usr/share/nginx/html/bundle.js.map.template > /usr/share/nginx/html/bundle.js.map
 
 exec nginx -g "daemon off;"
