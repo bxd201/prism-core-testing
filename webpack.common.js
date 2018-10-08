17708c009a4d838e7e7ebc402b24891da472d0b8
Daniel Richmond <cody.richmond@sherwin.com>
2018-10-08 10:51:03 -0400

    Updating CSS var extraction to A) catch some units that weren't being extracted before (namely decimals and percentages). Breaking SASS mixins out into separate files.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
 const path = require( 'path' );
-const _ = require( 'lodash' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
@@ -10,6 +9,83 @@ const sassUtils = require( 'node-sass-utils' )( sass );
 const varValues = Object.freeze( require( __dirname + '/src/shared/variables.js' ).varValues );
 const varNames = Object.freeze( require( __dirname + '/src/shared/variables.js' ).varNames );
 const memoizee = require( 'memoizee' );
+
+
+const getVarGenerator = ( function() {
+  const cssUnits = [
+    'rem',
+    'em',
+    'vh',
+    'vw',
+    'vmin',
+    'vmax',
+    'ex',
+    '%',
+    'px',
+    'cm',
+    'mm',
+    'in',
+    'pt',
+    'pc',
+    'ch'
+  ];
+
+  // look for anything starting with a whole number or decimal and ending in one of the above units
+  const unitRegex = new RegExp( `^([0-9.]+)(${cssUnits.join('|')})$` );
+
+  const convertStringToSassDimension = memoizee( function convertStringToSassDimension( str ) {
+    // Only attempt to convert strings
+    if( typeof str !== 'string' ) {
+      return str;
+    }
+
+    const result = unitRegex.exec( str );
+
+    if( result ) {
+      const value = result[ 1 ];
+      const unit = result[ 2 ];
+
+      return new sassUtils.SassDimension( parseFloat( value, 10 ), unit );
+    }
+
+    return str;
+  }, { primitive: true, length: 1 } );
+
+  function processVarData( data ) {
+    var type = typeof data,
+      returner;
+
+    // Convert to SassDimension if dimenssion
+    if( type === 'string' ) {
+      returner = convertStringToSassDimension( data );
+    } else if( type === 'object' ) {
+      // if it's an object, we'll need to recursively iterate through it
+      returner = {};
+      Object.keys( data ).forEach( function( key ) {
+        returner[ key ] = processVarData( data[ key ] );
+      } );
+    }
+
+    return returner;
+  }
+
+  return function getVarGenerator( values ) {
+    const getVarInternal_memoized = memoizee( function getVarInternal( keys ) {
+      var _keys = keys.split( '.' ), returner, i;
+
+      returner = Object.assign( {}, values );
+      
+      for( i = 0; i < _keys.length; i++ ) {
+        returner = returner[ _keys[ i ] ];
+      }
+
+      return sassUtils.castToSass( processVarData( returner ) );
+    }, { primitive: true, length: 1 } );
+    
+    return ( keys ) => getVarInternal_memoized( keys.getValue() );
+  }
+} )();
+
 
 
 const sassRules = [
@@ -26,6 +102,8 @@ const sassRules = [
     }
   }
 ];
+
+
 
 module.exports = {
   entry: {
@@ -76,71 +154,3 @@ module.exports = {
     }
   }
 };
-
-
-const convertStringToSassDimension = memoizee( function convertStringToSassDimension( result ) {
-  // Only attempt to convert strings
-  if( typeof result !== 'string' ) {
-    return result;
-  }
-
-  const cssUnits = [
-    'rem',
-    'em',
-    'vh',
-    'vw',
-    'vmin',
-    'vmax',
-    'ex',
-    '%',
-    'px',
-    'cm',
-    'mm',
-    'in',
-    'pt',
-    'pc',
-    'ch'
-  ];
-  const parts = result.match( /[a-zA-Z]+|[0-9]+/g );
-  const value = parts[ 0 ];
-  const unit = parts[ parts.length - 1 ];
-
-  if( cssUnits.indexOf( unit ) !== -1 ) {
-    return new sassUtils.SassDimension( parseInt( value, 10 ), unit );
-  }
-
-  return result;
-}, { primitive: true, length: 1 } );
-
-function processVarData( result ) {
-  var returner;
-
-  // Convert to SassDimension if dimenssion
-  if( typeof result === 'string' ) {
-    returner = convertStringToSassDimension( result );
-  } else if( typeof result === 'object' ) {
-    // if it's an object, we'll need to recursively iterate through it
-    returner = {};
-    Object.keys( result ).forEach( function( key ) {
-      returner[ key ] = processVarData( result[ key ] );
-    } );
-  }
-
-  return returner;
-}
-
-function getVarGenerator( values ) {
-  const getVarInternal_memoized = memoizee( function getVarInternal( keys ) {
-    var _keys = keys.split( '.' ), returner, i;
-
-    returner = Object.assign( {}, values );
-    
-    for( i = 0; i < _keys.length; i++ ) {
-      returner = returner[ _keys[ i ] ];
-    }
-
-    return sassUtils.castToSass( processVarData( returner ) );
-  }, { primitive: true, length: 1 } );
-  
-  return ( keys ) => getVarInternal_memoized( keys.getValue() );
-}
