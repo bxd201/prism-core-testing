ee58534f24141a167ae516ecb01c5cdc6c38219a
Brendan Do <brendan.do@sherwin.com>
2018-10-18 10:33:50 -0400

    Fixing the endpoints build

--- ci/scripts/entrypoint.build.sh
+++ ci/scripts/entrypoint.build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp ci/templates/endpoints.jsx.template src/constants/endpoints.jsx
+cp ci/templates/endpoints.js.template src/constants/endpoints.js
 NODE_ENV=development npm install || exit $?
 npm run build || exit $?
 tar zcf dist.tgz dist
