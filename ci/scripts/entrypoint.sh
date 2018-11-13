c0662755c4f76ebbc252e87babfb687af0e37065
Brendan Do <brendan.do@sherwin.com>
2018-11-13 11:24:12 -0500

    build | adding the remainder of the entry point JS files to be envsub'd & removing the unnecessary POST rule in the nginx conf

--- ci/scripts/entrypoint.sh
+++ ci/scripts/entrypoint.sh
@@ -3,4 +3,10 @@
 envsubst '$API_URL:$WEB_URL' < /usr/share/nginx/html/bundle.js.template > /usr/share/nginx/html/bundle.js
 envsubst '$API_URL:$WEB_URL' < /usr/share/nginx/html/bundle.js.map.template > /usr/share/nginx/html/bundle.js.map
 
+envsubst '$API_URL:$WEB_URL' < /usr/share/nginx/html/author.js.template > /usr/share/nginx/html/author.js
+envsubst '$API_URL:$WEB_URL' < /usr/share/nginx/html/author.js.map.template > /usr/share/nginx/html/author.js.map
+
+envsubst '$API_URL:$WEB_URL' < /usr/share/nginx/html/embed.js.template > /usr/share/nginx/html/embed.js
+envsubst '$API_URL:$WEB_URL' < /usr/share/nginx/html/embed.js.map.template > /usr/share/nginx/html/embed.js.map
+
 exec nginx -g "daemon off;"
