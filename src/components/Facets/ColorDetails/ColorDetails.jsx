b85ba4b2f120b0b7c91e8f012bc4d47c0ce0cf1c
Preston Williams <prwilliams@sherwin.com>
2019-01-03 17:53:05 -0500

    Color Details - put back some of Cody's work that I had accidentally removed

--- src/components/Facets/ColorDetails/ColorDetails.jsx
+++ src/components/Facets/ColorDetails/ColorDetails.jsx
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import { flatten, filter } from 'lodash'
+import { has } from 'lodash'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
 import { FormattedMessage } from 'react-intl'
 
@@ -27,7 +27,7 @@ type State = {
   chipIsMaximized: boolean
 }
 
-class ColorDetails extends PureComponent<Props> {
+class ColorDetails extends PureComponent<Props, State> {
   static baseClass = 'color-info'
 
   state: State = {
@@ -46,8 +46,17 @@ class ColorDetails extends PureComponent<Props> {
     const { match: { params }, colors } = this.props
     const { sceneIsDisplayed, chipIsMaximized } = this.state
 
+    // TODO: Color Details won't be a top level component, so this may not be valid so temporarily not rendering until it has colors
+    if (!colors) {
+      return null
+    }
+
     // grab the color by color number from the URL
-    const activeColor = this.getColorByColorNumber(params.colorNumber)
+    const activeColor = this.getColorById(params.colorId)
+    if (!activeColor) {
+      console.info(`ColorDetails: ${params.colorId} is not a valid color ID`)
+      return null
+    }
 
     const activeColorIsDark = activeColor.isDark
 
@@ -98,73 +107,74 @@ class ColorDetails extends PureComponent<Props> {
     }
 
     return (
-      <div className='color-detail-view'>
-        <div className={SWATCH_CLASSES.join(' ')} style={{ backgroundColor: activeColor.hex }} />
-        <div className={DISPLAY_TOGGLES_WRAPPER.join(' ')}>
-          <button className={SWATCH_SIZE_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleChipMaximized}>
-            <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fal', 'arrow-left']} color={contrastingTextColor} /><FontAwesomeIcon icon={['fal', 'arrow-right']} color={contrastingTextColor} />
-            <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='MAXIMIZE_COLOR_SWATCH' /></div>
-          </button>
-          <button className={ALT_SWATCH_SIZE_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleChipMaximized}>
-            <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fal', 'arrow-right']} color={contrastingTextColor} /><FontAwesomeIcon icon={['fal', 'arrow-left']} size='lg' color={contrastingTextColor} />
-            <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='RESTORE_COLOR_SWATCH_TO_DEFAULT_SIZE' /></div>
-          </button>
-        </div>
-        <div className={`color-detail__scene-wrapper ${sceneIsDisplayed ? ` color-detail__scene-wrapper--displayed` : ''}`}>
-          <SceneManager maxActiveScenes={1} />
-        </div>
-        <div className='color-detail__info-wrapper'>
-          <button className={SCENE_DISPLAY_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleSceneDisplay}>
-            <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fal', 'home']} color={contrastingTextColor} />
-            <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='DISPLAY_SCENE_PAINTER' /></div>
-          </button>
-          <button className={ALT_SCENE_DISPLAY_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleSceneDisplay}>
-            <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fas', 'home']} color={contrastingTextColor} />
-            <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='HIDE_SCENE_PAINTER' /></div>
-          </button>
+      <React.Fragment>
+        <div className='color-detail-view'>
+          <div className={SWATCH_CLASSES.join(' ')} style={{ backgroundColor: activeColor.hex }} />
+          <div className={DISPLAY_TOGGLES_WRAPPER.join(' ')}>
+            <button className={SWATCH_SIZE_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleChipMaximized}>
+              <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fal', 'arrow-left']} color={contrastingTextColor} /><FontAwesomeIcon icon={['fal', 'arrow-right']} color={contrastingTextColor} />
+              <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='MAXIMIZE_COLOR_SWATCH' /></div>
+            </button>
+            <button className={ALT_SWATCH_SIZE_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleChipMaximized}>
+              <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fal', 'arrow-right']} color={contrastingTextColor} /><FontAwesomeIcon icon={['fal', 'arrow-left']} size='lg' color={contrastingTextColor} />
+              <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='RESTORE_COLOR_SWATCH_TO_DEFAULT_SIZE' /></div>
+            </button>
+          </div>
+          <div className={`color-detail__scene-wrapper ${sceneIsDisplayed ? ` color-detail__scene-wrapper--displayed` : ''}`}>
+            <SceneManager maxActiveScenes={1} />
+          </div>
+          <div className='color-detail__info-wrapper'>
+            <button className={SCENE_DISPLAY_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleSceneDisplay}>
+              <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fal', 'home']} color={contrastingTextColor} />
+              <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='DISPLAY_SCENE_PAINTER' /></div>
+            </button>
+            <button className={ALT_SCENE_DISPLAY_TOGGLE_BUTTON_CLASSES.join(' ')} onClick={this.toggleSceneDisplay}>
+              <FontAwesomeIcon className={`${ColorDetails.baseClass}__display-toggles-icon`} icon={['fas', 'home']} color={contrastingTextColor} />
+              <div className={`${ColorDetails.baseClass}__scene-toggle-copy`}><FormattedMessage id='HIDE_SCENE_PAINTER' /></div>
+            </button>
 
-          <div className={MAIN_INFO_CLASSES.join(' ')} style={{ backgroundColor: activeColor.hex }}>
-            <ColorViewer color={activeColor} />
-            <ColorStrip key={activeColor.id} colors={colors} color={activeColor} />
-          </div>
-          <div className={`${ColorDetails.baseClass}__additional-info`}>
-            <Tabs>
-              <TabList className={`${ColorDetails.baseClass}__tab-list`} style={{ backgroundColor: activeColor.hex }}>
-                <Tab className={`${ColorDetails.baseClass}__tab`}>
-                  <div className={`${ColorDetails.baseClass}__tab-copy`}><FormattedMessage id='COORDINATING_COLORS' /></div>
-                </Tab>
-                <Tab className={`${ColorDetails.baseClass}__tab`}>
-                  <div className={`${ColorDetails.baseClass}__tab-copy`}><FormattedMessage id='SIMILAR_COLORS' /></div>
-                </Tab>
-                <Tab className={`${ColorDetails.baseClass}__tab`}>
-                  <div className={`${ColorDetails.baseClass}__tab-copy`}><FormattedMessage id='DETAILS' /></div>
-                </Tab>
-              </TabList>
-              <TabPanel className={`${ColorDetails.baseClass}__tab-panel`}>
-                <CoordinatingColors colors={colors} color={activeColor} />
-              </TabPanel>
-              <TabPanel className={`${ColorDetails.baseClass}__tab-panel`}>
-                <SimilarColors colors={colors} color={activeColor} />
-              </TabPanel>
-              <TabPanel className={`${ColorDetails.baseClass}__tab-panel color-info__tab-panel-details`}>
-                <ColorInfo color={activeColor} />
-              </TabPanel>
-            </Tabs>
+            <div className={MAIN_INFO_CLASSES.join(' ')} style={{ backgroundColor: activeColor.hex }}>
+              <ColorViewer color={activeColor} />
+              <ColorStrip key={activeColor.id} colors={colors} color={activeColor} />
+            </div>
+            <div className={`${ColorDetails.baseClass}__additional-info`}>
+              <Tabs>
+                <TabList className={`${ColorDetails.baseClass}__tab-list`} style={{ backgroundColor: activeColor.hex }}>
+                  <Tab className={`${ColorDetails.baseClass}__tab`}>
+                    <div className={`${ColorDetails.baseClass}__tab-copy`}><FormattedMessage id='COORDINATING_COLORS' /></div>
+                  </Tab>
+                  <Tab className={`${ColorDetails.baseClass}__tab`}>
+                    <div className={`${ColorDetails.baseClass}__tab-copy`}><FormattedMessage id='SIMILAR_COLORS' /></div>
+                  </Tab>
+                  <Tab className={`${ColorDetails.baseClass}__tab`}>
+                    <div className={`${ColorDetails.baseClass}__tab-copy`}><FormattedMessage id='DETAILS' /></div>
+                  </Tab>
+                </TabList>
+                <TabPanel className={`${ColorDetails.baseClass}__tab-panel`}>
+                  <CoordinatingColors colors={colors} color={activeColor} />
+                </TabPanel>
+                <TabPanel className={`${ColorDetails.baseClass}__tab-panel`}>
+                  <SimilarColors colors={colors} color={activeColor} />
+                </TabPanel>
+                <TabPanel className={`${ColorDetails.baseClass}__tab-panel color-info__tab-panel-details`}>
+                  <ColorInfo color={activeColor} />
+                </TabPanel>
+              </Tabs>
+            </div>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     )
   }
 
-  getColorByColorNumber (colorNumber) {
+  getColorById (colorId) {
     const { colors } = this.props
-    const color = filter(colors, color => color.colorNumber === colorNumber)[0]
 
-    if (!color) {
+    if (!has(colors, colorId)) {
       return null
     }
 
-    return color
+    return colors[colorId]
   }
 
   toggleSceneDisplay = function toggleSceneDisplay () {
@@ -179,11 +189,8 @@ class ColorDetails extends PureComponent<Props> {
 const mapStateToProps = (state, props) => {
   const { colors } = state
 
-  // TODO: WhooWhee, don't keep this. Need this here to be able to easily grab a color byId.
-  const colours = flatten(Object.keys(colors.items).map(fam => colors.items[fam]))
-
   return {
-    colors: colours
+    colors: colors.items.colorMap
   }
 }
 
