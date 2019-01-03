99790f5ecf699d51c6f0ceb4336a7b557b4a8d2d
Cody Richmond <cody.richmond@sherwin.com>
2019-01-03 10:16:36 -0500

    Flow fix for API_PATH and BASE_PATH globals.

--- src/shared/helpers/DataUtils.js
+++ src/shared/helpers/DataUtils.js
@@ -1,4 +1,6 @@
 // @flow
+import { memoize } from 'lodash'
+
 export function getByLowerCasePropName (obj: Object, propName: string): any {
   const realKey: string[] = Object.keys(obj).map(prop => prop.toLowerCase()).filter(prop => prop === propName)
   if (realKey.length && obj.hasOwnProperty(realKey[0])) {
@@ -13,6 +15,6 @@ export function getTotalWidthOf2dArray (arr: any[][]): number {
   return num
 }
 
-export function ensureFullyQualifiedAssetUrl (url: string) {
+export const ensureFullyQualifiedAssetUrl = memoize(function ensureFullyQualifiedAssetUrl (url: string): string {
   return (url.indexOf('scene7') > -1) ? url : `${BASE_PATH}${url}` // eslint-disable-line no-undef
-}
+})
