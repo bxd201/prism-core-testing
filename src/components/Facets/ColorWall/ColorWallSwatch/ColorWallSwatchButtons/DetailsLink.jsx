6cdd302d94e26d8058084d2cc5b77355d745c8e4
Brendan Do <brendan.do@sherwin.com>
2019-01-24 09:18:16 -0500

    Translating 'View Details'

--- src/components/Facets/ColorWall/ColorWallSwatch/ColorWallSwatchButtons/DetailsLink.jsx
+++ src/components/Facets/ColorWall/ColorWallSwatch/ColorWallSwatchButtons/DetailsLink.jsx
@@ -1,11 +1,12 @@
 // @flow
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FormattedMessage } from 'react-intl'
 
 const DetailsLink = ({ config, detailsLink, styles }: Object) => {
   if (config.ColorWall.displayViewDetails && detailsLink) {
     return (
-      <Link to={detailsLink} style={styles}>View Details</Link>
+      <Link to={detailsLink} style={styles}><FormattedMessage id='VIEW_DETAILS' /></Link>
     )
   }
   return null
