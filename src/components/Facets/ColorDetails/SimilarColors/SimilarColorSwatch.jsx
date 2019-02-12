6aa544de0fc21bbd4aa61b1cbd126703abd6df86
Brendan Do <brendan.do@sherwin.com>
2019-02-12 09:53:06 -0500

    Since none of the CDP sub-components needs to be stateful, refactoring all of them to be stateless components

--- src/components/Facets/ColorDetails/SimilarColors/SimilarColorSwatch.jsx
+++ src/components/Facets/ColorDetails/SimilarColors/SimilarColorSwatch.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import ReactGA from 'react-ga'
 
@@ -9,37 +9,26 @@ type Props = {
   color: Object
 }
 
-class SimilarColorSwatch extends PureComponent<Props> {
-  static baseClass = 'color-info'
-
-  constructor (props) {
-    super(props)
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick () {
+function SimilarColorSwatch ({ color }: Props) {
+  const BASE_CLASS = 'color-info'
+  const handleClick = () => {
     ReactGA.event({
       category: 'Color Detail / Similar Color',
       action: 'View Similar Color',
-      label: this.props.color.name
+      label: color.name
     })
   }
 
-  render () {
-    const { color } = this.props
-
-    return (
-      <li className={`${SimilarColorSwatch.baseClass}__similar-color`} style={{ backgroundColor: color.hex }}>
-        <Link to={generateColorDetailsPageUrl(color)} className={`${SimilarColorSwatch.baseClass}__similar-color-info`} onClick={this.handleClick} style={{ backgroundColor: color.hex }}>
-          <div className={`${SimilarColorSwatch.baseClass}__similar-color-info-wrapper`}>
-            <span className={`${SimilarColorSwatch.baseClass}__similar-color-brand-key`} >{color.brandKey}</span> <span className={`${SimilarColorSwatch.baseClass}__similar-color-number`} >{color.colorNumber}</span>
-            <p className={`${SimilarColorSwatch.baseClass}__similar-color-name`} >{color.name}</p>
-          </div>
-        </Link>
-      </li>
-    )
-  }
+  return (
+    <li className={`${BASE_CLASS}__similar-color`} style={{ backgroundColor: color.hex }}>
+      <Link to={generateColorDetailsPageUrl(color)} className={`${BASE_CLASS}__similar-color-info`} onClick={handleClick} style={{ backgroundColor: color.hex }}>
+        <div className={`${BASE_CLASS}__similar-color-info-wrapper`}>
+          <span className={`${BASE_CLASS}__similar-color-brand-key`} >{color.brandKey}</span> <span className={`${BASE_CLASS}__similar-color-number`} >{color.colorNumber}</span>
+          <p className={`${BASE_CLASS}__similar-color-name`} >{color.name}</p>
+        </div>
+      </Link>
+    </li>
+  )
 }
 
 export default SimilarColorSwatch
--- src/components/Facets/ColorDetails/SimilarColors/SimilarColors.jsx
+++ src/components/Facets/ColorDetails/SimilarColors/SimilarColors.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { FormattedMessage } from 'react-intl'
 
 import type { Color, ColorMap } from '../../../../shared/types/Colors'
@@ -11,37 +11,22 @@ type Props = {
   color: Color
 }
 
-type State = {
-  activeColorID: number | void
-}
+function SimilarColors ({ colors, color }: Props) {
+  const similarColors = color.similarColors
 
-class SimilarColors extends PureComponent<Props, State> {
-  static baseClass = 'color-info'
-
-  constructor (props: Props) {
-    super(props)
-
-    this.state = { activeColorID: void (0) }
-  }
-
-  render () {
-    const { colors, color } = this.props
-    const similarColors = color.similarColors
-
-    return (
-      <React.Fragment>
-        <h5 className='visually-hidden'><FormattedMessage id='SIMILAR_COLORS' /></h5>
-        <ul className={`${SimilarColors.baseClass}__similar-colors`}>
-          {similarColors.map((colorId: string) => {
-            const color = colors[colorId]
-            if (color) {
-              return <SimilarColorSwatch key={colorId} color={color} />
-            }
-          }).filter(color => !!color)}
-        </ul>
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      <h5 className='visually-hidden'><FormattedMessage id='SIMILAR_COLORS' /></h5>
+      <ul className={`color-info__similar-colors`}>
+        {similarColors.map((colorId: string) => {
+          const color = colors[colorId]
+          if (color) {
+            return <SimilarColorSwatch key={colorId} color={color} />
+          }
+        }).filter(color => !!color)}
+      </ul>
+    </React.Fragment>
+  )
 }
 
 export default SimilarColors
