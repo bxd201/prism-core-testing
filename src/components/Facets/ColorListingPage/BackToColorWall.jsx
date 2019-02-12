9af630b14f004c33178723a6886d8d74b3935d4b
Brendan Do <brendan.do@sherwin.com>
2019-02-12 13:08:28 -0500

    Switched the object types over to use the color flowtype and updating a couple more stateless components over

--- src/components/Facets/ColorListingPage/BackToColorWall.jsx
+++ src/components/Facets/ColorListingPage/BackToColorWall.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
 import { connect } from 'react-redux'
@@ -18,32 +18,28 @@ type OwnProps = {}
 
 type Props = StateProps & OwnProps
 
-class BackToColorWall extends PureComponent<Props> {
-  render () {
-    const { section, family, color } = this.props
+function BackToColorWall ({ color, family, section }: Props) {
+  // this will degrade gracefully if section and/or family and or color do(es) not exist in redux down to the root color wall URL
+  const colorWallUrl = color
+    ? generateColorWallPageUrl(section, family, color.id, fullColorName(color.brandKey, color.colorNumber, color.name))
+    : generateColorWallPageUrl(section, family)
 
-    // this will degrade gracefully if section and/or family and or color do(es) not exist in redux down to the root color wall URL
-    const colorWallUrl = color
-      ? generateColorWallPageUrl(section, family, color.id, fullColorName(color.brandKey, color.colorNumber, color.name))
-      : generateColorWallPageUrl(section, family)
-
-    return (
-      <div className='color-wall-mode-btns'>
-        <div className='color-wall-mode-btns__col'>
-          <div className='color-wall-mode-btns__cell'>
-            <FormattedMessage id='BACK_TO_COLOR_WALL'>
-              {(msg: string) => (
-                <Link to={colorWallUrl} className='color-wall-mode-btns__btn'>
-                  <FontAwesomeIcon icon={['fal', 'th-large']} pull='left' fixedWidth />
-                  {msg}
-                </Link>
-              )}
-            </FormattedMessage>
-          </div>
+  return (
+    <div className='color-wall-mode-btns'>
+      <div className='color-wall-mode-btns__col'>
+        <div className='color-wall-mode-btns__cell'>
+          <FormattedMessage id='BACK_TO_COLOR_WALL'>
+            {(msg: string) => (
+              <Link to={colorWallUrl} className='color-wall-mode-btns__btn'>
+                <FontAwesomeIcon icon={['fal', 'th-large']} pull='left' fixedWidth />
+                {msg}
+              </Link>
+            )}
+          </FormattedMessage>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = (state, props) => {
