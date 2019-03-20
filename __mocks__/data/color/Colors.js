0abf7c7a8370ce71d8341df2dec211aa47131667
Oliver Li <oliver.li@sherwin.com>
2019-03-20 12:36:14 -0400

    test creation for color details component

--- __mocks__/data/color/Colors.js
+++ __mocks__/data/color/Colors.js
@@ -5,106 +5,106 @@ import sample from 'lodash/sample'
 import type { ColorSetPayload, Color } from '../../../src/shared/types/Colors'
 
 export const Colors: ColorSetPayload = {
-  "Red": [
+  Red: [
     [
       {
-        "colorNumber": "6561",
-        "coordinatingColors": {
-          "coord2ColorId": 1926,
-          "coord1ColorId": 2250,
-          "whiteColorId": 2681
+        colorNumber: '6561',
+        coordinatingColors: {
+          coord2ColorId: 1926,
+          coord1ColorId: 2250,
+          whiteColorId: 2681
         },
-        "description": [
-          "Subdued",
-          "Muted",
-          "Brilliant",
-          "Radiant"
+        description: [
+          'Subdued',
+          'Muted',
+          'Brilliant',
+          'Radiant'
         ],
-        "id": "2248",
-        "isExterior": true,
-        "isInterior": true,
-        "name": "Teaberry",
-        "rgb": 15454683,
-        "lrv": 68.551,
-        "colorFamilyNames": [
-          "Red"
+        id: '2248',
+        isExterior: true,
+        isInterior: true,
+        name: 'Teaberry',
+        rgb: 15454683,
+        lrv: 68.551,
+        colorFamilyNames: [
+          'Red'
         ],
-        "brandKey": "SW",
-        "red": 235,
-        "green": 209,
-        "blue": 219,
-        "hue": 0.935897435897436,
-        "saturation": 0.39393939393939387,
-        "lightness": 0.8705882352941177,
-        "hex": "#ebd1db",
-        "isDark": false,
-        "cssrgb": "rgb(235,209,219)",
-        "storeStripLocator": "102-C1",
-        "similarColors": [
-          "2255",
-          "2262",
-          "2269",
-          "2662",
-          "2535",
-          "2276",
-          "2658",
-          "1974",
-          "2543",
-          "2531"
+        brandKey: 'SW',
+        red: 235,
+        green: 209,
+        blue: 219,
+        hue: 0.935897435897436,
+        saturation: 0.39393939393939387,
+        lightness: 0.8705882352941177,
+        hex: '#ebd1db',
+        isDark: false,
+        cssrgb: 'rgb(235,209,219)',
+        storeStripLocator: '102-C1',
+        similarColors: [
+          '2255',
+          '2262',
+          '2269',
+          '2662',
+          '2535',
+          '2276',
+          '2658',
+          '1974',
+          '2543',
+          '2531'
         ]
       },
       {
-        "colorNumber": "6568",
-        "coordinatingColors": {
-          "coord2ColorId": 11244,
-          "coord1ColorId": 2687,
-          "whiteColorId": 2681
+        colorNumber: '6568',
+        coordinatingColors: {
+          'coord2ColorId': 11244,
+          'coord1ColorId': 2687,
+          'whiteColorId': 2681
         },
-        "description": [
-          "Subdued",
-          "Muted",
-          "Brilliant",
-          "Radiant"
+        description: [
+          'Subdued',
+          'Muted',
+          'Brilliant',
+          'Radiant'
         ],
-        "id": "2255",
-        "isExterior": true,
-        "isInterior": true,
-        "name": "Lighthearted Pink",
-        "rgb": 15586781,
-        "lrv": 70.758,
-        "brandedCollectionIds": [
+        id: '2255',
+        isExterior: true,
+        isInterior: true,
+        name: 'Lighthearted Pink',
+        rgb: 15586781,
+        lrv: 70.758,
+        brandedCollectionIds: [
           31797,
           31791
         ],
-        "brandedCollectionNames": [
-          "Pottery Barn Fall/Winter 2018",
-          "Pottery Barn Fall/Winter 2018"
+        brandedCollectionNames: [
+          'Pottery Barn Fall/Winter 2018',
+          'Pottery Barn Fall/Winter 2018'
         ],
-        "colorFamilyNames": [
-          "Red"
+        colorFamilyNames: [
+          'Red'
         ],
-        "brandKey": "SW",
-        "red": 237,
-        "green": 213,
-        "blue": 221,
-        "hue": 0.9444444444444445,
-        "saturation": 0.3999999999999999,
-        "lightness": 0.8823529411764706,
-        "hex": "#edd5dd",
-        "isDark": false,
-        "cssrgb": "rgb(237,213,221)",
-        "storeStripLocator": "103-C1",
-        "similarColors": [
-          "2248",
-          "2262",
-          "2269",
-          "2276",
-          "1974",
-          "1981",
-          "2658",
-          "1995",
-          "2662",
-          "1988"
+        brandKey: 'SW',
+        red: 237,
+        green: 213,
+        blue: 221,
+        hue: 0.9444444444444445,
+        saturation: 0.3999999999999999,
+        lightness: 0.8823529411764706,
+        hex: '#edd5dd',
+        isDark: false,
+        cssrgb: 'rgb(237,213,221)',
+        storeStripLocator: '103-C1',
+        similarColors: [
+          '2248',
+          '2262',
+          '2269',
+          '2276',
+          '1974',
+          '1981',
+          '2658',
+          '1995',
+          '2662',
+          '1988'
         ]
       }
     ]
@@ -120,5 +120,14 @@ export function getColor (): Color | void {
   const fam1 = Object.keys(colors)[0]
   if (colors[fam1]) {
     return sample(colors[fam1][0])
+  }
+}
+
+export function getColorWithId (): {} | void {
+  const colors = getColors()
+  const fam1 = Object.keys(colors)[0]
+  if (colors[fam1]) {
+    console.log(colors[fam1][0][0])
+    return colors[fam1][0][0]
   }
 }
---
+++ __tests__/components/ColorDetails/CoordinatingColorSwatch.test.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import CoordinatingColorSwatch from 'src/components/Facets/ColorDetails/SimilarColors/SimilarColorSwatch'
+import * as Colors from '__mocks__/data/color/Colors'
+import ReactGA from 'react-ga'
+
+const color = Colors.getColor()
+let colors = [color.name, `${color.brandKey} ${color.colorNumber}`]
+
+const coordinatingColorSwatch = (color) => {
+  return shallow(<CoordinatingColorSwatch color={color} />)
+}
+
+// rendering test
+describe('Coordinary Color Swatch component with props as empty array', () => {
+  const wrapper = coordinatingColorSwatch([])
+
+  it('snapshot testing', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('component will be rendering', () => {
+    const colorSwatchExist = wrapper.exists()
+    expect(colorSwatchExist).toEqual(true)
+  })
+
+  it('component will not rendering color data inside of component', () => {
+    wrapper.find('p').forEach((el) => {
+      const colorExist = colors.includes(el.text())
+      expect(colorExist).toEqual(false)
+    })
+  })
+})
+
+describe('Coordinary Color Swatch component will pass props correctly', () => {
+  const wrapper = coordinatingColorSwatch(color)
+
+  it('snapshot testing', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('component will be rendering', () => {
+    const colorSwatchExist = wrapper.exists()
+    expect(colorSwatchExist).toEqual(true)
+  })
+
+  it('component will rendering color swatch data inside of component', () => {
+    wrapper.find('p').forEach((el) => {
+      const colorExist = colors.includes(el.text())
+      expect(colorExist).toEqual(true)
+    })
+  })
+})
+
+// event behavior test
+describe(('click event test of color swatch component'), () => {
+  const wrapper = coordinatingColorSwatch(color)
+  it('component should call handclick function', () => {
+    const handleClickMock = jest.fn()
+    ReactGA.event = handleClickMock
+    wrapper.find('Link').simulate('click')
+    expect(ReactGA.event).toHaveBeenCalled()
+  })
+})
