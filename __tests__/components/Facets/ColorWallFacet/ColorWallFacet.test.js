3d438ee08bd558146bf4d8cadfae36d0164d7329
pravin <pravin.jakate@sherwin.com>
2019-04-02 11:17:25 -0400

    add tests for ColorWallFacet

---
+++ __tests__/components/Facets/ColorWallFacet/ColorWallFacet.test.js
@@ -0,0 +1,61 @@
+/* eslint-env jest */
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Route, Switch } from 'react-router-dom'
+import { ColorWallFacet, RootRedirectColorWall } from 'src/components/Facets/ColorWallFacet/ColorWallFacet'
+import ColorWallRouteComponent from 'src/components/Facets/ColorWall/ColorWallRouteComponent'
+import { ROUTE_PARAMS } from 'constants/globals'
+
+const homeRoute = '/'
+const colorWallBaseUrl = `/${ROUTE_PARAMS.ACTIVE}/${ROUTE_PARAMS.COLOR_WALL}`
+const colorWallUrlPattern = `${colorWallBaseUrl}(/.*)?`
+const locationProp = { pathname: homeRoute }
+
+const getColorWallFacet = (props) => {
+  let defaultProps = {
+    location: locationProp
+  }
+
+  let newProps = Object.assign({}, defaultProps, props)
+  return shallow(<ColorWallFacet {...newProps} />)
+}
+
+describe('ColorWallFacet routes', () => {
+  let colorWallFacet
+  let pathMap = {}
+  beforeAll(() => {
+    if (!colorWallFacet) {
+      colorWallFacet = getColorWallFacet()
+    }
+
+    pathMap = colorWallFacet.find(Route).reduce((pathMap, route) => {
+      const routeProps = route.props()
+      pathMap[routeProps.path] = routeProps.component
+      return pathMap
+    }, {})
+  })
+
+  it('should match snapshot', () => {
+    expect(colorWallBaseUrl).toMatchSnapshot()
+  })
+
+  it('should render Switch component', () => {
+    expect(colorWallFacet.find(Switch).exists()).toBe(true)
+  })
+
+  it('should render Switch component with location prop defined as locationProp constant', () => {
+    expect(colorWallFacet.find(Switch).prop('location')).toEqual(locationProp)
+  })
+
+  it('should have 2 Route components', () => {
+    expect(colorWallFacet.find(Route)).toHaveLength(2)
+  })
+
+  it('should show RootRedirectColorWall component for route defined as homeRoute constant', () => {
+    expect(pathMap[homeRoute]).toBe(RootRedirectColorWall)
+  })
+
+  it('should show ColorWallRouteComponent component for route defined as colorWallUrlPattern constant', () => {
+    expect(pathMap[colorWallUrlPattern]).toBe(ColorWallRouteComponent)
+  })
+})
