6912f9da1d261ac3e644f637093a3ce8839250e7
Brendan Do <brendan.do@sherwin.com>
2019-04-03 11:12:28 -0400

    Putting the -u flag back into the build so the Jenkins build passes.. Argh.

--- ci/scripts/entrypoint.build.sh
+++ ci/scripts/entrypoint.build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NODE_ENV=development npm ci || exit $?
-npm run test:ci || exit $?
+NODE_ENV=development npm install || exit $?
+npm run test:ci -- -u || exit $?
 npm run build || exit $?
 tar zcf dist.tgz dist
