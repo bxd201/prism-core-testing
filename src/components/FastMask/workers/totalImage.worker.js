67382c67b0078915fffd87fdf6b243fb084253f8
Cody Richmond <cody.richmond@sherwin.com>
2019-07-12 16:20:08 -0400

    PRISM-181 | Implementing hue shift preservation to increase "realness" of color application. Also adding debug mode to FastMaskSVGDefs to help visually represent the masks we're creating.

--- src/components/FastMask/workers/totalImage.worker.js
+++ src/components/FastMask/workers/totalImage.worker.js
@@ -1,10 +1,12 @@
+/* eslint-disable */
 // @flow
 import mean from 'lodash/mean'
 import toNumber from 'lodash/toNumber'
+import clamp from 'lodash/clamp'
 import _ from 'lodash'
 
 import { significantFigures } from '../../../shared/helpers/DataUtils'
-import { tinycolor } from '../../../shared/helpers/ColorDataUtils'
+import { tinycolor, getHueRangeNumber, getDegreeDistance } from '../../../shared/helpers/ColorDataUtils'
 
 declare var self: DedicatedWorkerGlobalScope;
 
@@ -13,6 +15,9 @@ const ALPHA_THRESHOLD = 50
 // minimum allowed brightness value
 const LUMINANCE_MIN_VALUE = 0.15
 const LUMINANCE_MAX_VALUE = 0.75
+const IS_LIGHT_MIN_VALUE = 190
+const HUE_NORMALIZATION_STEP = 12
+const HUE_NORMALIZATION_STEP_DEG = 360 / HUE_NORMALIZATION_STEP
 
 self.addEventListener('message', (e: Object) => {
   const { image: img, masks } = e.data
@@ -34,16 +39,20 @@ self.addEventListener('message', (e: Object) => {
 
   setStatus(0)
 
+  // gather ALL our image data here; we'll refer to it later on a pixel-by-pixel basis to gather mask info
   runPerImagePixel(img, (i) => {
     const tc = tinycolor(`rgb(${img[i]}, ${img[i + 1]}, ${img[i + 2]})`)
+    const hsl = tc.toHsl()
     const bright = Math.round(tc.getBrightness()) // 0-255
     const lum = significantFigures(tc.getLuminance(), 4) // 4 signficant figures
-    const l = significantFigures(tc.toHsl().l, 2) // 3 signficant figures
+    const l = significantFigures(hsl.l, 2) // 3 signficant figures
+    const h = significantFigures(hsl.h, 2) // 3 signficant figures
 
     pixelData.push({
       brightness: bright,
       luminance: lum,
-      lightness: l
+      lightness: l,
+      hue: h
     })
   })
 
@@ -67,40 +76,62 @@ self.addEventListener('message', (e: Object) => {
 
   setStatus(1)
 
+  // ---------------------------------------------------------------------------
+  // BEGIN PER-SURFACE OPERATIONS
+  // ---------------------------------------------------------------------------
+
   masks.forEach((mask, i) => {
     const maskPixelData = []
+    const maskPixelIndices = []
+    const hueGroups = []
+    let maxLum = 0
+    let lumToAlphaMultiplier = 1
 
     setStatus(i + 1 + 0.25)
 
+    // TODO: store a map of masked pixel indices for faster future iteration?
     runPerImagePixel(mask, (i) => {
       const alpha = mask[i + 4]
 
       if (alpha >= ALPHA_THRESHOLD) {
         // matching index from our earlier pixelData collection
         const pixelIndex = Math.ceil(i / 4)
-        maskPixelData.push(pixelData[pixelIndex])
+        const thisMaskPixelData = pixelData[pixelIndex]
+        maskPixelIndices.push(pixelIndex)
+        maskPixelData.push(thisMaskPixelData)
+        maxLum = thisMaskPixelData.luminance > maxLum ? thisMaskPixelData.luminance : maxLum
       }
     })
+
+    const maskPixelLength = maskPixelIndices.length
 
     setStatus(i + 1 + 0.33)
 
     const meanLuminance = meanFrom(maskPixelData, 'luminance')
     const meanBrightness = meanFrom(maskPixelData, 'brightness')
     const meanLightness = meanFrom(maskPixelData, 'lightness')
+    const meanHue = meanFrom(maskPixelData, 'hue')
 
     setStatus(i + 1 + 0.5)
 
     const avgExtremeLuminance = avgExtremeFrom(maskPixelData, 'luminance')
     const avgExtremeBrightness = avgExtremeFrom(maskPixelData, 'brightness')
     const avgExtremeLightness = avgExtremeFrom(maskPixelData, 'lightness')
+    const avgExtremeHue = avgExtremeFrom(maskPixelData, 'hue')
 
     setStatus(i + 1 + 0.66)
 
     const medianLuminance = medianFrom(maskPixelData, 'luminance')
     const medianBrightness = medianFrom(maskPixelData, 'brightness')
     const medianLightness = medianFrom(maskPixelData, 'lightness')
+    const medianHue = medianFrom(maskPixelData, 'hue')
 
-    const hunchIsLight = avgExtremeBrightness.mostCommon > 190 && meanBrightness > 190 && medianBrightness > 190
+    // medianHue seems to really be our main color
+    // so pass that value through hue-grouper, multiply out to get our center hue category value
+    // then use that as a starting point to map out our hue mask
+    const normalizedMedianHue = getHueRangeNumber(medianHue, HUE_NORMALIZATION_STEP) * HUE_NORMALIZATION_STEP_DEG
+
+    const hunchIsLight = avgExtremeBrightness.mostCommon > IS_LIGHT_MIN_VALUE && meanBrightness > IS_LIGHT_MIN_VALUE && medianBrightness > IS_LIGHT_MIN_VALUE
     const hunchHasHighlight = ((lum, light) => {
       const { mostCommon, leastCommon } = lum
       // is the difference in luminosity extremes greater than .1?
@@ -125,68 +156,61 @@ self.addEventListener('message', (e: Object) => {
 
     setStatus(i + 1 + 0.75)
 
-    const { highlightData, luminanceThreshold } = (() => {
-      if (hunchHasHighlight) {
-        const LUMINANCE_THRESHOLD = _(maskPixelData)
-          .chain()
-          .map(v => v.luminance)
-          .groupBy(v => `${v}`)
-          .mapValues(a => significantFigures(a.length, 2))
-          .thru(obj => Object.keys(obj).map(p => {
-            return {
-              luminance: parseFloat(p),
-              count: obj[p]
-            }
-          }))
-          .sortBy('luminance')
-          .filter(v => v.luminance > LUMINANCE_MIN_VALUE)
-          .reduce((pv, nv) => pv && pv.count > nv.count ? pv : nv, void (0))
-          .get('luminance')
-          .clamp(LUMINANCE_MIN_VALUE, LUMINANCE_MAX_VALUE)
-          .thru(v => isNaN(v) ? LUMINANCE_MIN_VALUE : v)
-          .value()
+    if (hunchHasHighlight) {
+      // this value is used to ensure highlighted areas are being masked more completely based on luminance
+      lumToAlphaMultiplier = 1 / maxLum
+    }
 
-        // console.log('Surface luminance threshold detected:', LUMINANCE_THRESHOLD)
+    const { highlightData, luminanceThreshold, hueData } = (() => {
+      const luminanceThreshold = 0.8 * (medianLuminance + avgExtremeLuminance.mostCommon) / 2
+      const highlightData = new Uint8ClampedArray(mask.length)
+      const hueData = new Uint8ClampedArray(mask.length)
 
-        const highlightData = new Uint8ClampedArray(mask.length)
+      maskPixelIndices.forEach(i => {
+        const realIndex = i * 4
+        const alpha = mask[i + 4]
 
-        runPerImagePixel(mask, (i) => {
-          const alpha = mask[i + 4]
+        // matching index from our earlier pixelData collection
+        const imagePixel = pixelData[i]
 
-          if (alpha >= ALPHA_THRESHOLD) {
-            // matching index from our earlier pixelData collection
-            const pixelIndex = Math.ceil(i / 4)
-            const imagePixel = pixelData[pixelIndex]
-            const lum = imagePixel.luminance
+        // BEGIN LUM STUFF
+        if (hunchHasHighlight) {
+          const lum = imagePixel.luminance
 
-            if (lum > LUMINANCE_THRESHOLD) {
-              const alpha = parseInt(((lum - LUMINANCE_THRESHOLD) / (1 - LUMINANCE_THRESHOLD)) * 255, 10)
+          if (lum > luminanceThreshold) {
+            const alpha = Math.min(parseInt(((lum - luminanceThreshold) / (1 - luminanceThreshold)) * 255 * lumToAlphaMultiplier, 10), 255)
 
-              highlightData[i] = 255
-              highlightData[i + 1] = 255
-              highlightData[i + 2] = 255
-              highlightData[i + 3] = alpha
-
-              return
-            }
+            highlightData[realIndex] = 255
+            highlightData[realIndex + 1] = 255
+            highlightData[realIndex + 2] = 255
+            highlightData[realIndex + 3] = alpha
           }
-
-          highlightData[i] = 0
-          highlightData[i + 1] = 0
-          highlightData[i + 2] = 0
-          highlightData[i + 3] = 0
-        })
-
-        return {
-          luminanceThreshold,
-          // pass only the buffer and reassemble into the correct view later to POTENTIALLY improve performance
-          highlightData: highlightData.buffer
         }
-      }
+        // END LUM STUFF
+
+        // BEGIN HUE STUFF
+        const h = imagePixel.hue
+        // get distance between this hue and our normalized median
+        const dist = getDegreeDistance(normalizedMedianHue, h)
+        // how much of the spectrum is considered the intended wall color
+        const sameSteps = 2
+        // how much of the spectrum beyond the intended wall color will be considered a transition stage before no desaturation is applied
+        const transSteps = 2
+        const isSame = dist <= (sameSteps * HUE_NORMALIZATION_STEP_DEG)
+        const isTrans = !isSame && dist <= ((sameSteps + transSteps) * HUE_NORMALIZATION_STEP_DEG)
+        const hueAlpha = isSame ? 1 : isTrans ? (1 - clamp((dist - (HUE_NORMALIZATION_STEP_DEG * sameSteps)) / (HUE_NORMALIZATION_STEP_DEG * transSteps), 0, 1)) : 0
+
+        hueData[realIndex] = 255
+        hueData[realIndex + 1] = 255
+        hueData[realIndex + 2] = 255
+        hueData[realIndex + 3] = parseInt(hueAlpha * 255, 10)
+      })
 
       return {
-        luminanceThreshold: void (0),
-        highlightData: void (0)
+        luminanceThreshold,
+        hueData: hueData.buffer,
+        // pass only the buffer and reassemble into the correct view later to POTENTIALLY improve performance
+        highlightData: highlightData.buffer
       }
     })()
 
@@ -194,14 +218,19 @@ self.addEventListener('message', (e: Object) => {
       meanLuminance,
       meanBrightness,
       meanLightness,
+      meanHue,
       avgExtremeLuminance,
       avgExtremeBrightness,
       avgExtremeLightness,
+      avgExtremeHue,
       medianLuminance,
       medianBrightness,
       medianLightness,
+      medianHue,
+      normalizedMedianHue,
       luminanceThreshold,
       highlightMap: highlightData,
+      hueMap: hueData,
       hunches: {
         hasHighlight: hunchHasHighlight,
         isLight: hunchIsLight
@@ -271,7 +300,9 @@ function avgExtremeFrom (collection, prop) {
 
   return {
     mostCommon: significantFigures(mean(values.slice(quarter * 3)), 3),
-    leastCommon: significantFigures(mean(values.slice(0, quarter)), 3)
+    leastCommon: significantFigures(mean(values.slice(0, quarter)), 3),
+    loMid: significantFigures(mean(values.slice(quarter, quarter * 2)), 3),
+    hiMid: significantFigures(mean(values.slice(quarter * 2, quarter * 3)), 3)
   }
 }
 
