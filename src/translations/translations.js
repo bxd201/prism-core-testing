febd909c6e51df3958fcb45e8660a121ed407756
Brendan Do <brendan.do@sherwin.com>
2019-09-16 14:12:54 -0400

    misc | updating react non-problematic dependencies
    misc | more dependency updates

--- src/translations/translations.js
+++ src/translations/translations.js
@@ -1,19 +1,7 @@
-import { addLocaleData } from 'react-intl'
-
-// import all supported locales
-import localeEN from 'react-intl/locale-data/en'
-import localeFR from 'react-intl/locale-data/fr'
-
 // import all supported languages
 import enUS from './en-US.json'
 import enCA from './en-CA.json'
 import frCA from './fr-CA.json'
-
-// add locale data when using react-intl to format numbers/times/ect..
-addLocaleData([
-  ...localeEN,
-  ...localeFR
-])
 
 // list all supported languages & associate with their JSON
 const languages = {
