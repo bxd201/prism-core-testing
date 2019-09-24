fb1357f0811300505f64c9fdcb3bd6fab8075ad8
Brendan Do <brendan.do@sherwin.com>
2019-09-24 10:21:17 -0400

    TAG | changes to support passing in a CDP root URL for linking to the CDP from the digital color wall

--- src/components/Facets/ColorWall/ColorWallSwatch/ColorWallSwatchButtons/DetailsLink.jsx
+++ src/components/Facets/ColorWall/ColorWallSwatch/ColorWallSwatchButtons/DetailsLink.jsx
@@ -2,9 +2,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FormattedMessage } from 'react-intl'
+import kebabCase from 'lodash/kebabCase'
 
 const DetailsLink = (props: Object) => {
-  const { config, detailsLink, ...other } = props
+  const { config, detailsLink, color, ...other } = props
+
+  // TODO: Future scope, the below shouldn't be driven by a data attribute, but should come in from the config as a eval capable string that
+  // a utility method perhaps can handle per brand.
+  if (config.colorDetailPageRoot) {
+    return (
+      <a href={`${config.colorDetailPageRoot}/${color.brandKey}${color.colorNumber}-${kebabCase(color.name)}`} {...other}>
+        <FormattedMessage id='VIEW_DETAILS' />
+      </a>
+    )
+  }
+
   if (config.displayDetailsLink && detailsLink) {
     return (
       <Link to={detailsLink} {...other}>
@@ -12,6 +24,7 @@ const DetailsLink = (props: Object) => {
       </Link>
     )
   }
+
   return null
 }
 
