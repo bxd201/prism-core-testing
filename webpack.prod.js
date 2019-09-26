466bdec910472a41d11c172430d9a9c935c7f93c
Brendan Do <brendan.do@sherwin.com>
2019-09-26 14:00:13 -0400

    build | fixing a addition to trailing slash that was added by accident and removing the deletion of HTML files for replatform so we can test during the replatform development phase

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
+// TODO: brendan.do
+// Uncomment the clean plugin when we merge back into develop since we don't want the HTML files
+// going out onto production. But it needs to exist during the replatform testing.
+
 const merge = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const WebpackCleanPlugin = require('webpack-clean')
+// const WebpackCleanPlugin = require('webpack-clean')
 
 const common = require('./webpack.common.js')
 
@@ -10,11 +14,11 @@ module.exports = merge(
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-      new CleanWebpackPlugin(),
-      new WebpackCleanPlugin([
-        'dist/index.html',
-        'dist/embeddable.html'
-      ])
+      new CleanWebpackPlugin()
+      // new WebpackCleanPlugin([
+      //   'dist/index.html',
+      //   'dist/embeddable.html'
+      // ])
     ]
   }
 )
