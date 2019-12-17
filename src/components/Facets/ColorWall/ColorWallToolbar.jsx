dbb2c667f3881306de6c53d8221d53b7d1fd61be
Cody Richmond <cody.richmond@sherwin.com>
2019-12-17 11:52:18 -0500

    PRISM-345 | Ensuring "cancel" button presence in color wall facet when viewing color families.

--- src/components/Facets/ColorWall/ColorWallToolbar.jsx
+++ src/components/Facets/ColorWall/ColorWallToolbar.jsx
@@ -12,17 +12,17 @@ const PATH_END_FAMILY = 'family/'
 export default () => {
   const { path, params: { section, family } } = useRouteMatch()
   const { sections = [], families = [] } = useSelector(state => state.colors)
-  const isFamilyView = path.endsWith(PATH_END_FAMILY)
+  const isFamilyView = !!family || path.endsWith(PATH_END_FAMILY)
 
   return (
     <div className={MODE_CLASS_NAMES.BASE}>
       <div className={MODE_CLASS_NAMES.COL}>
         <div className={MODE_CLASS_NAMES.CELL}>
           <ButtonBar.Bar>
-            {(isFamilyView || family ? families : sections).map(name =>
+            {(isFamilyView ? families : sections).map(name =>
               <ButtonBar.Button
                 key={name}
-                to={isFamilyView || family ? generateColorWallPageUrl(section, name) : generateColorWallPageUrl(name)}
+                to={isFamilyView ? generateColorWallPageUrl(section, name) : generateColorWallPageUrl(name)}
               >
                 <span className={MODE_CLASS_NAMES.DESC}>{name}</span>
               </ButtonBar.Button>
@@ -31,18 +31,22 @@ export default () => {
         </div>
         <div className={`${MODE_CLASS_NAMES.CELL} ${MODE_CLASS_NAMES.RIGHT}`}>
           <ButtonBar.Bar>
-            {!isFamilyView && <ButtonBar.Button disabled={families.length <= 1} to={`${generateColorWallPageUrl(section)}${PATH_END_FAMILY}`}>
-              <FontAwesomeIcon className='color-families-svg' icon={['fa', 'palette']} pull='left' />
-              <span className={MODE_CLASS_NAMES.DESC}><FormattedMessage id='COLOR_FAMILIES' /></span>
-            </ButtonBar.Button>}
-            {!isFamilyView && <ButtonBar.Button to={`${generateColorWallPageUrl(section, family)}search/`}>
-              <FontAwesomeIcon className='color-families-svg' icon={['fa', 'search']} pull='left' />
-              <span className={MODE_CLASS_NAMES.DESC}><FormattedMessage id='SEARCH.SEARCH' /></span>
-            </ButtonBar.Button>}
-            {isFamilyView && <ButtonBar.Button to={generateColorWallPageUrl(section)}>
-              <FontAwesomeIcon className='close-icon-svg' icon={['fa', 'times']} pull='left' />
-              <span className={MODE_CLASS_NAMES.DESC}><FormattedMessage id='CANCEL' /></span>
-            </ButtonBar.Button>}
+            {!isFamilyView && <>
+              <ButtonBar.Button disabled={families.length <= 1} to={`${generateColorWallPageUrl(section)}${PATH_END_FAMILY}`}>
+                <FontAwesomeIcon className='color-families-svg' icon={['fa', 'palette']} pull='left' />
+                <span className={MODE_CLASS_NAMES.DESC}><FormattedMessage id='COLOR_FAMILIES' /></span>
+              </ButtonBar.Button>
+              <ButtonBar.Button to={`${generateColorWallPageUrl(section, family)}search/`}>
+                <FontAwesomeIcon className='color-families-svg' icon={['fa', 'search']} pull='left' />
+                <span className={MODE_CLASS_NAMES.DESC}><FormattedMessage id='SEARCH.SEARCH' /></span>
+              </ButtonBar.Button>
+            </>}
+            {!!isFamilyView && <>
+              <ButtonBar.Button to={generateColorWallPageUrl(section)}>
+                <FontAwesomeIcon className='close-icon-svg' icon={['fa', 'times']} pull='left' />
+                <span className={MODE_CLASS_NAMES.DESC}><FormattedMessage id='CANCEL' /></span>
+              </ButtonBar.Button>
+            </>}
           </ButtonBar.Bar>
         </div>
       </div>
