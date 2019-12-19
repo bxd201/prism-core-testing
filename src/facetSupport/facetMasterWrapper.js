371e8e222899437d357c8404145ca6817c3a529c
Cody Richmond <cody.richmond@sherwin.com>
2019-12-19 10:55:35 -0500

    PRISM-326 | Styling generic ErrorBoundary. Adding ability for user to read in details about error.

--- src/facetSupport/facetMasterWrapper.js
+++ src/facetSupport/facetMasterWrapper.js
@@ -3,7 +3,7 @@ import React, { type ComponentType, useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { IntlProvider } from 'react-intl'
 import { BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom'
-import ErrorBoundary from 'src/helpers/ErrorBoundary'
+import ErrorBoundary from 'src/helpers/ErrorBoundary/ErrorBoundary'
 import { initTrackingOnce } from './facetTracking'
 import { type EmbeddedConfiguration } from 'src/shared/types/Configuration'
 import ConfigurationContextProvider from 'src/contexts/ConfigurationContext/ConfigurationContextProvider'
@@ -61,16 +61,21 @@ export const facetMasterWrapper = (Component: ComponentType<any>) => {
 
     const flatLanguages = flattenNestedObject(languages[language])
 
+    // Two levels of error boundary here. The vast majority of errors will hit the inner boundary, which will provide translated messaging.
+    // If, for some reason, an error occurs in the IntlProvider (or ErrorBoundary) level, the outer boundary will catch it instead and at least display SOMETHING, although
+    // that something will include our message keys.
     return (
-      <ErrorBoundary>
+      <ErrorBoundary translated={false}>
         <IntlProvider locale={language} messages={flatLanguages} textComponent={React.Fragment}>
-          <Provider store={store}>
-            <ConfigurationContextProvider {...props}>
-              <LiveAnnouncer>
-                { RouterRender }
-              </LiveAnnouncer>
-            </ConfigurationContextProvider>
-          </Provider>
+          <ErrorBoundary>
+            <Provider store={store}>
+              <ConfigurationContextProvider {...props}>
+                <LiveAnnouncer>
+                  { RouterRender }
+                </LiveAnnouncer>
+              </ConfigurationContextProvider>
+            </Provider>
+          </ErrorBoundary>
         </IntlProvider>
       </ErrorBoundary>
     )
