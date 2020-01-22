31b9aa5d4c032c2a91f2abbc43f6f9b963bac6d0
Cody Richmond <cody.richmond@sherwin.com>
2020-01-22 10:18:00 -0500

    Build | Removing InspiredScene and MatchPhoto from Prism facet because both of these components import large amounts of static data, which does not minify and bloats our build files.
    Before reintegrating these components with the Prism facet, please remove dependencies on static data. TODO: Fully integrate with redux and build some transformation methods as needed to support these components.

--- src/components/Facets/Prism/PrismNav.jsx
+++ src/components/Facets/Prism/PrismNav.jsx
@@ -13,9 +13,9 @@ const isColorWall = (pathname) => {
   return /^\/active\/color-wall/.test(pathname)
 }
 
-const isColorFromImage = (pathname) => {
-  return /^\/color-from-image/.test(pathname)
-}
+// const isColorFromImage = (pathname) => {
+//   return /^\/color-from-image/.test(pathname)
+// }
 
 const isColorCollections = (pathname) => {
   return /^\/color-collections/.test(pathname)
@@ -25,13 +25,13 @@ const isExpertColor = (pathname) => {
   return /^\/expert-colors/.test(pathname)
 }
 
-const isMatchPhoto = (pathname) => {
-  return /^\/match-photo/.test(pathname)
-}
+// const isMatchPhoto = (pathname) => {
+//   return /^\/match-photo/.test(pathname)
+// }
 
-const isPaintScene = (pathname) => {
-  return /^\/paint-scene/.test(pathname)
-}
+// const isPaintScene = (pathname) => {
+//   return /^\/paint-scene/.test(pathname)
+// }
 
 const isFastMask = (pathname) => {
   return /^\/fast-mask/.test(pathname)
@@ -48,11 +48,11 @@ export default () => {
     <>
       <Link to='/active' className={`prism-nav-btn ${isScene(pathname) ? 'prism-nav-btn--active' : ''}`}>Scenes</Link>
       <Link to='/active/color-wall/section/sherwin-williams-colors' className={`prism-nav-btn ${isColorWall(pathname) ? 'prism-nav-btn--active' : ''}`}>Color Wall</Link>
-      <Link to='/color-from-image' className={`prism-nav-btn ${isColorFromImage(pathname) ? 'prism-nav-btn--active' : ''}`}>Color From Image</Link>
+      {/* <Link to='/color-from-image' className={`prism-nav-btn ${isColorFromImage(pathname) ? 'prism-nav-btn--active' : ''}`}>Color From Image</Link> */}
       <Link to='/color-collections' className={`prism-nav-btn ${isColorCollections(pathname) ? 'prism-nav-btn--active' : ''}`}>Color Collections</Link>
       <Link to='/expert-colors' className={`prism-nav-btn ${isExpertColor(pathname) ? 'prism-nav-btn--active' : ''}`}>Expert Colors</Link>
-      <Link to='/match-photo' className={`prism-nav-btn ${isMatchPhoto(pathname) ? 'prism-nav-btn--active' : ''}`}>Match Photo</Link>
-      <Link to='/paint-scene' className={`prism-nav-btn ${isPaintScene(pathname) ? 'prism-nav-btn--active' : ''}`}>Paint a Scene</Link>
+      {/* <Link to='/match-photo' className={`prism-nav-btn ${isMatchPhoto(pathname) ? 'prism-nav-btn--active' : ''}`}>Match Photo</Link> */}
+      {/* <Link to='/paint-scene' className={`prism-nav-btn ${isPaintScene(pathname) ? 'prism-nav-btn--active' : ''}`}>Paint a Scene</Link> */}
       <Link to='/fast-mask' className={`prism-nav-btn ${isFastMask(pathname) ? 'prism-nav-btn--active' : ''}`}>Fast Mask</Link>
     </>
   )
