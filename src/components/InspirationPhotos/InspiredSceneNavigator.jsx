cc0533f53c1b083a7aa7c99984a32e8324d28e57
Justin Gottshall <justin.gottshall@sherwin.com>
2020-01-23 08:44:02 -0500

    Fixes issues with the Carousel

--- src/components/InspirationPhotos/InspiredSceneNavigator.jsx
+++ src/components/InspirationPhotos/InspiredSceneNavigator.jsx
@@ -1,115 +1,49 @@
 // @flow
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import CollectionsHeaderWrapper from '../CollectionsHeaderWrapper/CollectionsHeaderWrapper'
 import ColorCollectionsTab from '../Shared/ColorCollectionsTab'
-import ImageScenesWithCarousel from './InspiredScene'
-import React from 'react'
-
-import { connect } from 'react-redux'
-import { loadInspirationalPhotos as loadData } from '../../store/actions/inspirationalPhotos'
-
+import InspiredScene from './InspiredScene'
+import Carousel from '../Carousel/Carousel'
+import { loadInspirationalPhotos } from '../../store/actions/inspirationalPhotos'
 import '../ColorCollections/ColorCollections.scss'
 
 const baseClass = 'color-collections'
-type ComponentProps = {
-  collectionTabs: Array,
-  flatData: Array,
-  loadData: Function,
-  setHeader: Function,
-  tabMap: Object
-}
 
-type ComponentState = {
-  tabId: string
-}
+type ComponentProps = { setHeader: Function }
 
-export class InspiredSceneNavigator extends React.Component<ComponentProps, ComponentState> {
-  constructor (props) {
-    super(props)
+const InspiredSceneNavigator = ({ setHeader }: ComponentProps) => {
+  const dispatch = useDispatch()
+  useEffect(() => { loadInspirationalPhotos()(dispatch) }, [])
 
-    this.state = {
-      tabId: 'tab0',
-      key: 'tab0' + Date.now().toString()
-    }
-  }
+  const { collectionTabs, flatData, tabMap } = useSelector(({ inspirationalPhotos: { data } }) => ({
+    collectionTabs: data.flatMap((scene, i) => ({ id: `tab${i}`, tabName: scene.name })),
+    tabMap: data.flatMap((scene, i) => Array(scene.sceneDefinition.length).fill(`tab${i}`)),
+    flatData: data.flatMap((scene, i) => (
+      scene.sceneDefinition.map((def, x) => ({ img: def.photoUrl, id: `tab${i}-${x}`, initPins: def.initPins }))
+    ))
+  }))
 
-  componentDidMount () {
-    this.props.loadData()
-    const headerContent = 'Inspirational Photos'
-    this.props.setHeader(headerContent)
-  }
+  const [tabId: string, setTabId: string => void] = useState('tab0')
 
-  showTab = (id: string) => {
-    this.setState({
-      tabId: id,
-      rotatedFlatData: this.rotateArray(
-        this.props.flatData,
-        Object.values(this.props.tabMap).indexOf(id)
-      )
-    })
-  }
+  useEffect(() => { setHeader('Inspirational Photos') }, [])
 
-  rotateArray = (arr, index) => {
-    return arr.slice(index).concat(arr.slice(0, index))
-  }
-
-  render () {
-    return (
-      <div className={`${baseClass}__wrapper`}>
-        <ColorCollectionsTab
-          collectionTabs={this.props.collectionTabs}
-          showTab={this.showTab}
-          tabIdShow={this.state.tabId}
+  return (
+    <div className={`${baseClass}__wrapper`}>
+      <ColorCollectionsTab collectionTabs={collectionTabs} tabIdShow={tabId} showTab={setTabId} />
+      <div className={`${baseClass}__collections-list`}>
+        <Carousel
+          BaseComponent={InspiredScene}
+          data={flatData}
+          defaultItemsPerView={1}
+          tabId={tabId}
+          setTabId={setTabId}
+          isInfinity
+          tabMap={tabMap}
         />
-        <div className={`${baseClass}__collections-list`}>
-          <ImageScenesWithCarousel
-            data={this.state.rotatedFlatData || this.props.flatData}
-            defaultItemsPerView={1}
-            isInfinity
-            showTab={this.showTab}
-            tabMap={this.props.tabMap}
-          />
-        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapStateToProps = (state) => {
-  const // eslint-disable-line one-var
-    { inspirationalPhotos = { data: [] } } = state,
-    tabMap = [],
-    collectionTabs = [],
-    flatData = inspirationalPhotos.data.flatMap(
-      (scene, i) => (
-        // update collectionTabs
-        collectionTabs.push({ id: `tab${i}`, tabName: scene.name }), // eslint-disable-line no-sequences
-        // update tabMaps
-        tabMap.push(Array(scene.sceneDefinition.length).fill(`tab${i}`)),
-        // create flatData
-        scene.sceneDefinition.map((def, x) => ({
-          img: def.photoUrl,
-          id: `tab${i}-${x}`,
-          initPins: def.initPins
-        }))
-      )
-    )
-
-  return {
-    collectionTabs,
-    flatData,
-    tabMap: tabMap
-      .flat()
-      .reduce((acc, cur, i) => (acc[i] = cur, acc), {}) // eslint-disable-line no-return-assign, no-sequences
-  }
-}
-
-const mapDispatchToProps = (dispatch: Function) => {
-  return {
-    loadData () { dispatch(loadData()) }
-  }
-}
-
-export default CollectionsHeaderWrapper(connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(InspiredSceneNavigator))
+export default CollectionsHeaderWrapper(InspiredSceneNavigator)
