ff5b70081291db6dde4d3a01a0194787aa95c761
Cody Richmond <cody.richmond@sherwin.com>
2020-02-10 15:04:05 -0500

    PRISM-480 | Refactoring ImagePreloader to behave more predictably.
    No longer passing through and augmenting props in ImagePreloader to a child element prop. Image preloader now returns a function with error and loading properties to now feed into a real child.

--- src/helpers/ImagePreloader.jsx
+++ src/helpers/ImagePreloader.jsx
@@ -1,5 +1,5 @@
 // @flow
-import React, { PureComponent } from 'react'
+import { useState, useEffect } from 'react'
 import isEmpty from 'lodash/isEmpty'
 import flattenDeep from 'lodash/flattenDeep'
 
@@ -8,132 +8,115 @@ import { type NestedArray } from '../shared/types/Common.js.flow'
 import MaskObj from '../store/masks/MaskObj'
 
 type Props = {
-  // $FlowIgnore
-  el: React$ElementClass,
+  children: Function => any,
   preload: NestedArray<string | MaskObj | void>
 }
 
-type State = {
-  error: boolean,
-  loading: boolean
-}
+const promiseMap: {
+  [ key: string ]: Promise<any>
+} = {}
 
-class ImagePreloader extends PureComponent<Props, State> {
-  state: State = {
-    error: false,
-    loading: false
-  }
+const ImagePreloader = (props: Props) => {
+  const { children, preload } = props
 
-  static promiseMap: {
-    [ key: string ]: Promise<any>
-  } = {}
-
-  static getPromise = function (key: string): ?Promise<any> {
-    return ImagePreloader.promiseMap[key]
-  }
-
-  static performDownload (path: string, resolve: {
-    using: Function,
-    with?: any
-  }, reject: {
-    using: Function
-  }, final: boolean = false) {
-    var img = new Image()
-
-    img.onload = () => {
-      resolve.using(resolve.with)
-    }
-
-    img.onerror = (err) => {
-      if (final) {
-        console.info(`Failed to download ${path}. No more retries available. Error reference:`, err)
-        reject.using(err)
-        return false
-      }
-
-      console.info(`Failed to download ${path}. Retrying download. Error reference:`, err)
-      setTimeout(() => {
-        ImagePreloader.performDownload(path, resolve, reject, true)
-      }, 10000)
-    }
-
-    // set the source
-    img.src = path
-  }
-
-  static makePromise = function (tgt: string | MaskObj): Promise<any> | typeof undefined {
-    let existingPromise
-
-    if (tgt instanceof MaskObj) {
-      existingPromise = ImagePreloader.getPromise(tgt.id)
-    } else if (typeof tgt === 'string') {
-      existingPromise = ImagePreloader.getPromise(ensureFullyQualifiedAssetUrl(tgt))
-    }
-
-    if (existingPromise) {
-      return existingPromise
-    }
-
-    if (tgt instanceof MaskObj) {
-      ImagePreloader.promiseMap[tgt.id] = tgt.loading
-      return tgt.loading
-    } else if (typeof tgt === 'string') {
-      const newPromise = new Promise((resolve: Function, reject: Function) => {
-        const fullPath = ensureFullyQualifiedAssetUrl(tgt)
-        // non-blocking request, unlike setting Image source
-        ImagePreloader.performDownload(
-          fullPath,
-          {
-            using: resolve,
-            with: fullPath
-          }, {
-            using: reject
-          }
-        )
-      })
-
-      ImagePreloader.promiseMap[tgt] = newPromise
-
-      return newPromise
-    }
-  }
-
-  constructor (props: Props) {
-    super(props)
-
-    const { preload } = this.props
-
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
     if (!isEmpty(preload)) {
       const flatPreload = flattenDeep(preload).filter(val => !!val)
 
       if (flatPreload.length) {
-        Promise.all(flatPreload.map(tgt => ImagePreloader.makePromise(tgt))).then((response: any) => {
-          this.setState({
-            error: false,
-            loading: false
-          })
+        Promise.all(flatPreload.map(tgt => makePromise(tgt))).then((response: any) => {
+          setError(false)
+          setLoading(false)
         }).catch((error: any) => {
-          this.setState({
-            error: true,
-            loading: false
-          })
+          setError(true)
+          setLoading(false)
           console.warn(error)
         })
 
-        this.state.loading = true
+        setLoading(true)
       } else {
-        this.state.error = false
-        this.state.loading = false
+        setLoading(false)
+        setError(false)
       }
     }
+  }, [preload])
+
+  return children({ error, loading })
+}
+
+// -----------------------------------------------
+// IMAGE DOWNLOAD MANAGEMENT FUNCTIONS
+// -----------------------------------------------
+
+function getPromise (key: string): ?Promise<any> {
+  return promiseMap[key]
+}
+
+function performDownload (path: string, resolve: {
+  using: Function,
+  with?: any
+}, reject: {
+  using: Function
+}, final: boolean = false) {
+  var img = new Image()
+
+  img.onload = () => {
+    resolve.using(resolve.with)
   }
 
-  render () {
-    // remove el and preload because they are specific to ImagePreloader -- we'll pass the other props along
-    const { el, preload, ...other } = this.props
-    const { error, loading } = this.state
+  img.onerror = (err) => {
+    if (final) {
+      console.info(`Failed to download ${path}. No more retries available. Error reference:`, err)
+      reject.using(err)
+      return false
+    }
 
-    return React.createElement(el, Object.assign({}, other, { error: error, loading: loading }))
+    console.info(`Failed to download ${path}. Retrying download. Error reference:`, err)
+    setTimeout(() => {
+      performDownload(path, resolve, reject, true)
+    }, 10000)
+  }
+
+  // set the source
+  img.src = path
+}
+
+function makePromise (tgt: string | MaskObj): Promise<any> | typeof undefined {
+  let existingPromise
+
+  if (tgt instanceof MaskObj) {
+    existingPromise = getPromise(tgt.id)
+  } else if (typeof tgt === 'string') {
+    existingPromise = getPromise(ensureFullyQualifiedAssetUrl(tgt))
+  }
+
+  if (existingPromise) {
+    return existingPromise
+  }
+
+  if (tgt instanceof MaskObj) {
+    promiseMap[tgt.id] = tgt.loading
+    return tgt.loading
+  } else if (typeof tgt === 'string') {
+    const newPromise = new Promise((resolve: Function, reject: Function) => {
+      const fullPath = ensureFullyQualifiedAssetUrl(tgt)
+      // non-blocking request, unlike setting Image source
+      performDownload(
+        fullPath,
+        {
+          using: resolve,
+          with: fullPath
+        }, {
+          using: reject
+        }
+      )
+    })
+
+    promiseMap[tgt] = newPromise
+
+    return newPromise
   }
 }
 
