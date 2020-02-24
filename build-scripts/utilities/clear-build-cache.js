df13327aedbe21bd53f0ea46bfe50fd9d26f5d35
Cody Richmond <cody.richmond@sherwin.com>
2020-02-24 14:28:58 -0500

    Build | Specifically targeting hardsource cache location when running cleanstart.

--- build-scripts/utilities/clear-build-cache.js
+++ build-scripts/utilities/clear-build-cache.js
@@ -14,11 +14,13 @@ function deleteFolderRecursive (path) {
 
     console.log(`Deleting directory "${path}"...`)
     fs.rmdirSync(path)
+  } else {
+    console.log('(nothing to delete)')
   }
 };
 
 console.log('Clearing hard-source cache...')
-deleteFolderRecursive('./cache')
+deleteFolderRecursive('./.cache/hard-source')
 console.log('Successfully cleared hard-source cache!')
 
 console.log('Clearing terser cache...')
