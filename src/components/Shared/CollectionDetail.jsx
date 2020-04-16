79e8c84b01d3bf7452160b6103a7d32a5105cd13
Justin Gottshall <justin.gottshall@sherwin.com>
2020-04-16 15:07:47 -0400

    PRISM-646 | color wall rewrite (#818)
    PRISM-646 | color wall rewrite

--- src/components/Shared/CollectionDetail.jsx
+++ src/components/Shared/CollectionDetail.jsx
@@ -1,33 +1,19 @@
 // @flow
 import React, { useRef } from 'react'
-import { connect } from 'react-redux'
 // $FlowIgnore -- no defs for react-virtualized
 import { Grid, AutoSizer } from 'react-virtualized'
-import ColorWallSwatch from '../Facets/ColorWall/ColorWallSwatch/ColorWallSwatch'
+import ColorSwatch from 'src/components/Facets/ColorWall/ColorSwatch/ColorSwatch'
 import ColorWallContext, { colorWallContextDefault } from '../Facets/ColorWall/ColorWallContext'
-import { add } from '../../store/actions/live-palette'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import 'src/providers/fontawesome/fontawesome'
 import * as scroll from 'scroll'
 import './CollectionDetail.scss'
 import 'src/scss/externalComponentSupport/AutoSizer.scss'
-import type { ColorCollectionDetail, Color } from '../../shared/types/Colors.js.flow'
+import type { ColorCollectionDetail } from '../../shared/types/Colors.js.flow'
 
 const GRID_AUTOSCROLL_SPEED: number = 300
 
-type Props = {
-  collectionDetailData: ColorCollectionDetail,
-  addToLivePalette: Function
-}
-
 type Ref = RefObject
-
-const cwProviderValues = {
-  ...colorWallContextDefault,
-  displayDetailsLink: false,
-  displayInfoButton: true,
-  displayAddButton: true
-}
 
 const baseClass = 'collection-detail'
 const wrapper = `${baseClass}__wrapper`
@@ -41,91 +27,67 @@ const verticalControlsTrigger = `${baseClass}__trigger`
 const triggerPrevious = `${baseClass}__previous-trigger`
 const triggerNext = `${baseClass}__next-trigger`
 
-function cellRenderer ({
-  columnIndex, // Horizontal (column) index of cell
-  isScrolling, // The Grid is currently being scrolled
-  isVisible, // This cell is visible within the grid (eg it is not an overscanned cell)
-  key, // Unique key within array of cells
-  parent, // Reference to the parent Grid (instance)
-  rowIndex, // Vertical (row) index of cell
-  style // Style object to be applied to cell (to position it)
-}: Object) {
-  const { props: { columnCount, addToLivePalette, colors } } = parent
-  const index = columnIndex + (rowIndex * columnCount)
-
-  if (colors && colors[index]) {
-    const thisColor: Color = colors[index]
-
-    return (
-      <div key={key} style={style}>
-        <ColorWallContext.Provider value={cwProviderValues}>
-          <ColorWallSwatch
-            key={thisColor.hex}
-            showContents
-            color={thisColor}
-            onAdd={addToLivePalette}
-          />
-        </ColorWallContext.Provider>
-      </div>
-    )
-  }
-
-  return null
-}
-
 let _gridHeight = 0
 let _cellSize = 0
 function handleGridResize ({ width, height }) {
   _gridHeight = height
 }
 
-export const CollectionDetail = (props: Props) => {
-  const resultSwatchSize = 175
-  const { addToLivePalette, collectionDetailData } = props
+type Props = { collectionDetailData: ColorCollectionDetail, addToLivePalette: Function }
+export const CollectionDetail = ({ addToLivePalette, collectionDetailData }: Props) => {
   const _gridWrapperRef: ?Ref = useRef(null)
+  const resultSwatchSize = 175
+
+  const cellRenderer = ({ columnIndex, isScrolling, isVisible, key, parent, rowIndex, style }) => {
+    const { columnCount, colors } = parent.props
+    const index = columnIndex + (rowIndex * columnCount)
+    return colors && colors[index] && <ColorSwatch key={key} style={style} color={colors[index]} showContents />
+  }
 
   return (
-    <div className={`${wrapper}`}>
-      <div className={`${collectionInfo}`}>
-        <img className={`${collectionCover}`} alt='' src={`${collectionDetailData.img}`} />
-        <div className={`${collectionDescription}`}>{collectionDetailData.description}</div>
-      </div>
-      <div className={`${collectionColorList}`}>
-        <div ref={_gridWrapperRef} className={`${collectionColorListVertical}`}>
-          <AutoSizer onResize={handleGridResize}>
-            {({ height, width }) => {
-              const collectionSize = collectionDetailData.collections.length
-              const columnCount = Math.max(1, Math.round(width / resultSwatchSize))
-              const rowCount = Math.ceil(collectionSize / columnCount)
-              const newSize = width / columnCount
-              _cellSize = newSize
+    <ColorWallContext.Provider value={{ ...colorWallContextDefault, displayDetailsLink: false, displayInfoButton: true, displayAddButton: true }}>
+      <div className={`${wrapper}`}>
+        <div className={`${collectionInfo}`}>
+          <img className={`${collectionCover}`} alt='' src={`${collectionDetailData.img}`} />
+          <div className={`${collectionDescription}`}>{collectionDetailData.description}</div>
+        </div>
+        <div className={`${collectionColorList}`}>
+          <div ref={_gridWrapperRef} className={`${collectionColorListVertical}`}>
+            <AutoSizer onResize={handleGridResize}>
+              {({ height, width }) => {
+                const collectionSize = collectionDetailData.collections.length
+                const columnCount = Math.max(1, Math.round(width / resultSwatchSize))
+                const rowCount = Math.ceil(collectionSize / columnCount)
+                const newSize = width / columnCount
+                _cellSize = newSize
 
-              return (
-                <Grid
-                  colors={collectionDetailData.collections}
-                  cellRenderer={cellRenderer}
-                  columnWidth={newSize}
-                  columnCount={columnCount}
-                  height={height}
-                  rowHeight={newSize}
-                  rowCount={rowCount}
-                  width={width}
-                  addToLivePalette={addToLivePalette}
-                />
-              )
-            }}
-          </AutoSizer>
-        </div>
-        <div className={`${collectionVerticalControls}`}>
-          <div role='presentation' onClick={() => verticalScroll(_gridWrapperRef, 'top', _gridHeight, _cellSize)} className={`${verticalControlsTrigger} ${triggerPrevious}`}>
-            <FontAwesomeIcon className={``} icon={['fa', 'angle-up']} />
+                return (
+                  <Grid
+                    colors={collectionDetailData.collections}
+                    cellRenderer={cellRenderer}
+                    columnWidth={newSize}
+                    columnCount={columnCount}
+                    height={height}
+                    rowHeight={newSize}
+                    rowCount={rowCount}
+                    width={width}
+                    addToLivePalette={addToLivePalette}
+                  />
+                )
+              }}
+            </AutoSizer>
           </div>
-          <div role='presentation' onClick={() => verticalScroll(_gridWrapperRef, 'bottom', _gridHeight, _cellSize)} className={`${verticalControlsTrigger} ${triggerNext}`}>
-            <FontAwesomeIcon className={``} icon={['fa', 'angle-down']} />
+          <div className={`${collectionVerticalControls}`}>
+            <div role='presentation' onClick={() => verticalScroll(_gridWrapperRef, 'top', _gridHeight, _cellSize)} className={`${verticalControlsTrigger} ${triggerPrevious}`}>
+              <FontAwesomeIcon className={``} icon={['fa', 'angle-up']} />
+            </div>
+            <div role='presentation' onClick={() => verticalScroll(_gridWrapperRef, 'bottom', _gridHeight, _cellSize)} className={`${verticalControlsTrigger} ${triggerNext}`}>
+              <FontAwesomeIcon className={``} icon={['fa', 'angle-down']} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </ColorWallContext.Provider>
   )
 }
 
@@ -147,18 +109,5 @@ function verticalScroll (_gridWrapperRef, direction, _gridHeight, _cellSize) {
   }
 }
 
-const mapDispatchToProps = (dispatch: Function) => {
-  return {
-    addToLivePalette: (color) => {
-      dispatch(add(color))
-    }
-  }
-}
-
-export {
-  collectionCover,
-  collectionDescription,
-  triggerPrevious,
-  triggerNext
-}
-export default connect(null, mapDispatchToProps)(CollectionDetail)
+export { collectionCover, collectionDescription, triggerPrevious, triggerNext }
+export default CollectionDetail
