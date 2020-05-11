ba6f48fbfb7ad5e060ca59a50d35317d63154813
Justin Gottshall <justin.gottshall@sherwin.com>
2020-05-11 15:02:36 -0400

    PRISM-701 | show an info icon for the 'show details' button on EDE colors

--- src/components/InfoButton/InfoButton.jsx
+++ src/components/InfoButton/InfoButton.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { type Color } from 'src/shared/types/Colors.js.flow'
 import { showColorDetailsModal } from 'src/store/actions/loadColors'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import filter from 'lodash/filter'
 import values from 'lodash/values'
 import './InfoButton.scss'
@@ -12,11 +13,11 @@ export default ({ color }: InfoButtonProps) => {
   const colors: Color[] = useSelector(store => filter(store.colors.items.colorMap, c => values(color.coordinatingColors).some(id => id === c.id)))
   const dispatch = useDispatch()
 
-  return colors.length > 0 && (
-    <button className='info-button' onClick={() => dispatch(showColorDetailsModal(color))}>
-      {colors.map((color: Color, index: number) => {
-        return <span key={index} className='info-button-color-strip' style={{ backgroundColor: color.hex, borderTop: index !== 0 ? 0 : '1px solid #333' }} />
-      })}
+  return (
+    <button className={'info-button' + (colors.length > 0 ? '' : ' outlined')} onClick={() => dispatch(showColorDetailsModal(color))}>
+      {colors.length > 0
+        ? colors.map((color: Color, index: number) => <span key={index} className='info-button-color-strip' style={{ backgroundColor: color.hex, borderTop: index !== 0 ? 0 : '1px solid #333' }} />)
+        : <FontAwesomeIcon icon={['fas', 'info']} size='1x' />}
     </button>
   )
 }
