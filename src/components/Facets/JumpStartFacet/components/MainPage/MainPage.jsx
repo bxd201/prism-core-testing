5eb071ea27151646e68b9266696ca4debc30bec2
Justin Gottshall <justin.gottshall@sherwin.com>
2020-09-11 07:28:59 -0400

    JUMP-9 | updates MainPage/ImageUpload page

--- src/components/Facets/JumpStartFacet/components/MainPage/MainPage.jsx
+++ src/components/Facets/JumpStartFacet/components/MainPage/MainPage.jsx
@@ -1,56 +1,55 @@
 // @flow
-import React, { useMemo, useCallback } from 'react'
+import React, { useRef, useState } from 'react'
 import { useDropzone } from 'react-dropzone'
-import uniqueId from 'lodash/uniqueId'
-
-import StyledFileBtn from '../StyledFileBtn/StyledFileBtn'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/pro-solid-svg-icons'
+import { faLongArrowUp } from '@fortawesome/pro-regular-svg-icons'
 import 'src/scss/convenience/visually-hidden.scss'
 import './MainPage.scss'
 
-const baseClass = 'JSFMainPage'
-
 type MainPageProps = { onSelectFile: Function }
 
-function MainPage (props: MainPageProps) {
-  const { onSelectFile } = props
-  const id = useMemo(() => uniqueId('jsf-main-file-upload'), [])
+function MainPage ({ onSelectFile }: MainPageProps) {
+  const hiddenFileInput: { current: ?HTMLInputElement } = useRef()
+  const [file, setFile] = useState()
+  const { getRootProps, getInputProps } = useDropzone({ onDrop: (files = []) => files.length && setFile(files[0]) })
 
-  const handleFiles = useCallback((files = []) => {
-    if (files.length) {
-      onSelectFile(files[0])
-    }
-  }, [])
-
-  const handleChangeFiles = useCallback(e => {
-    const { target: { files = [] } } = e
-    e.stopPropagation()
-    handleFiles(Array.from(files))
-  })
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop: handleFiles })
-
-  return <div className={baseClass}>
-    <div className='JSFCommon__band JSFCommon__band--pad'>
-      <div className='JSFCommon__content JSFCommon__content--centered JSFCommon__text'>
-        <p>We'll recommend colors that look good in your space and products that perform the way that you need.</p>
-        <p>All we need from you is one image.</p>
+  return (
+    <div className='jumpstart-image-upload'>
+      <input className='visually-hidden' type='file' ref={hiddenFileInput} onChange={e => e.target.files.length && setFile(e.target.files[0])} />
+      <div className='centered-content'>
+        <h1>Jumpstart</h1>
+        <p>You're just a few moments away from our best product and color recommendations— customized to your space</p>
+        <h2>To get started, let's see your space.</h2>
+        {file
+          ? (
+            <>
+              <img src={URL.createObjectURL(file)} alt='user uploaded' />
+              <button className='upload-new-image' onClick={() => hiddenFileInput.current && hiddenFileInput.current.click()}>
+                <FontAwesomeIcon icon={faLongArrowUp} size='2x' transform='shrink-7' mask={faCircle} />
+                <strong>Upload new image</strong>
+              </button>
+            </>
+          )
+          : (
+            <>
+              <form {...getRootProps({ onClick: event => event.stopPropagation() })}>
+                <input {...getInputProps({ multiple: false })} />
+                <div>
+                  <FontAwesomeIcon icon={faLongArrowUp} size='3x' transform='shrink-7' mask={faCircle} />
+                  <p><strong>Drag and drop your image here</strong></p>
+                  <p>or</p>
+                  <button onClick={() => hiddenFileInput.current && hiddenFileInput.current.click()}><strong>Select a file</strong></button>
+                </div>
+              </form>
+              <p className='note'><strong>Note:</strong> Jumpstart performs best on photos with ample, even lighting</p>
+            </>
+          )
+        }
+        <button className='submit-button' disabled={file === undefined} onClick={() => onSelectFile(file)}>Submit</button>
       </div>
     </div>
-    <div className='JSFCommon__band JSFCommon__band--dark'>
-      <form {...getRootProps({
-        tabIndex: -1,
-        onClick: event => event.stopPropagation(),
-        className: `${baseClass}__dropzone ${isDragActive ? `${baseClass}__dropzone--active` : ''}`
-      })}>
-        <input multiple={false} className='visually-hidden' onChange={handleChangeFiles} id={id} tabIndex='0' type='file' accept={null} data-noshow />
-        <input {...getInputProps({ multiple: false })} />
-
-        <p>Drag your image here</p>
-        <p>or <StyledFileBtn id={id} text='select a file' /> to upload</p>
-      </form>
-    </div>
-  </div>
+  )
 }
 
 export default MainPage
