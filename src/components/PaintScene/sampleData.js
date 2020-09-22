f5aa65bb25754e6bd45223ee1bd9fb450bcf04d8
Oliver Li <oliver.li@sherwin.com>
2020-09-22 00:58:06 -0400

    add resize for separate masking

--- src/components/PaintScene/sampleData.js
+++
@@ -1,1 +0,0 @@
-export const maskingPink = { red: '255', green: '44', blue: '180', hex: '#fc2cb3' }
--- src/components/PaintScene/utils.js
+++ src/components/PaintScene/utils.js
@@ -894,3 +894,5 @@ export const getColorsForMergeColors = (workspace: PaintSceneWorkspace) => {
     return { r: 255, g: 255, b: 255 }
   })
 }
+
+export const maskingPink = { red: '255', green: '44', blue: '180', hex: '#fc2cb3' }
