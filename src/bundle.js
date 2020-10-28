048ccc9948dc63411507759d75de5efcbb019519
Cody Richmond <cody.richmond@sherwin.com>
2020-10-28 12:58:03 -0400

    PRISM-864 | Cleaning up test output after fixing env var substitution.

--- src/bundle.js
+++ src/bundle.js
@@ -3,8 +3,6 @@ import 'src/allFacets' // import all facets so they're included in the bundle
 import { embedAtRoots, embedAtElement } from 'src/facetSupport/facetBinder'
 import updateGlobalPrismObject from 'src/facetSupport/utils/updateGlobalPrismObject'
 
-console.info('test BASE_URL value in bundle:', BASE_PATH)
-
 // expose embed method on global PRISM object in order to manually call this later
 updateGlobalPrismObject('__embed', embedAtElement)
 
