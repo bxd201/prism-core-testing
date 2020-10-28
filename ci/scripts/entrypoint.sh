ee7138dd6ded83f48b61126a597ca7430fb9d309
Daniel Richmond <cody.richmond@sherwin.com>
2020-10-28 11:51:48 -0400

    PRISM-864 | Continuing to address build issues relating to env var substitution

--- ci/scripts/entrypoint.sh
+++ ci/scripts/entrypoint.sh
@@ -8,6 +8,8 @@ for file_path in $(ls -1 /app/templates/*.js.template); do
   envsubst '$API_URL:$WEB_URL' < /app/templates/${file}.template > /app/config/${file}
 done
 
+mkdir -p /app/config/js
+
 # Loop over all template files in root/js
 for file_path in $(ls -1 /app/templates/js/*.js.template); do
   file=$(basename ${file_path} .template)
