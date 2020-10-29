0437d5690996d2666bbfd5dd3c7f8768fe05c6b7
Justin Gottshall <justin.gottshall@sherwin.com>
2020-10-29 12:07:30 -0400

    PRISM-893 | adds a spanish translations file, switches the language of the LAD CVW template to es-AR

--- src/translations/translations.js
+++ src/translations/translations.js
@@ -2,12 +2,14 @@
 import enUS from './en-US.json'
 import enCA from './en-CA.json'
 import frCA from './fr-CA.json'
+import esAR from './es-AR.json'
 
 // list all supported languages & associate with their JSON
 const languages = {
   'en-US': enUS,
   'en-CA': enCA,
-  'fr-CA': frCA
+  'fr-CA': frCA,
+  'es-AR': esAR
 }
 
 export default languages
