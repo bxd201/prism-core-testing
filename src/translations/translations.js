3d9bcb3c1d0639fac239293f330d3ed85ed1859d
Justin Gottshall <justin.gottshall@sherwin.com>
2020-11-06 12:45:36 -0500

    adds es-EC as a language option, adds a new LAD-CVW template for that language

--- src/translations/translations.js
+++ src/translations/translations.js
@@ -3,13 +3,15 @@ import enUS from './en-US.json'
 import enCA from './en-CA.json'
 import frCA from './fr-CA.json'
 import esAR from './es-AR.json'
+import esEC from './es-EC.json'
 
 // list all supported languages & associate with their JSON
 const languages = {
   'en-US': enUS,
   'en-CA': enCA,
   'fr-CA': frCA,
-  'es-AR': esAR
+  'es-AR': esAR,
+  'es-EC': esEC
 }
 
 export default languages
