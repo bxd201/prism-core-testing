c256584c41cd0b77ba0a2b2d316a006bba861c35
Justin Gottshall <justin.gottshall@sherwin.com>
2020-11-15 20:25:55 -0500

    prevent shrinking color wall down to 0 columns

--- src/components/Facets/ColorWall/ColorWallUtils.js
+++ src/components/Facets/ColorWall/ColorWallUtils.js
@@ -120,7 +120,7 @@ export const makeChunkGrid = memoizee((unChunkedChunks: number[][], chunkGridPar
   const wrapChunks = (chunkedChunks: string[][][]): string[][][][] => {
     return wrappingEnabled
       // determine gridWidth by decreasing it until it is less than the containerWidth
-      ? chunk(chunkedChunks, rangeRight(gridWidth + 1).find(w => getWidthOfWidestChunkRowInChunkGrid(chunk(chunkedChunks, w)) < containerWidth - 3))
+      ? chunk(chunkedChunks, rangeRight(0, gridWidth + 1).find(w => getWidthOfWidestChunkRowInChunkGrid(chunk(chunkedChunks, w)) < containerWidth - 3))
       : chunk(chunkedChunks, gridWidth)
   }
 
