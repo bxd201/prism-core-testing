2da9257beefaed2ade854003021f9f9626502e00
Fabio General <fabio.general.silva@sherwin.com>
2021-06-16 12:18:41 -0400

    CBGLCM-43 | Added color details viewer subtitles feature exclusion

--- src/components/Facets/ColorDetails/ColorViewer.jsx
+++ src/components/Facets/ColorDetails/ColorViewer.jsx
@@ -1,16 +1,18 @@
 // @flow
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-
 import type { Color } from '../../../shared/types/Colors.js.flow'
-
 import { fullColorNumber } from '../../../shared/helpers/ColorUtils'
+import WithConfigurationContext from '../../../contexts/ConfigurationContext/WithConfigurationContext'
+import { shouldAllowFeature } from '../../../shared/utils/featureSwitch.util'
+import { FEATURE_EXCLUSIONS } from '../../../constants/configurations'
 
 type Props = {
-  color: Color
+  color: Color,
+  config: { featureExclusions?: string[] },
 }
 
-function ColorViewer ({ color }: Props) {
+function ColorViewer ({ color, config }: Props) {
   const BASE_CLASS = 'color-info'
 
   return (
@@ -19,18 +21,22 @@ function ColorViewer ({ color }: Props) {
         <span className={`${BASE_CLASS}__number`}>{fullColorNumber(color.brandKey, color.colorNumber)}</span>
         <span className={`${BASE_CLASS}__name`}>{color.name}</span>
       </h1>
-      <h2 className={`${BASE_CLASS}__type`}>
-        {(color.isInterior) ? <FormattedMessage id='INTERIOR' /> : ''}
-        {(color.isInterior && color.isExterior) ? ' / ' : ''}
-        {(color.isExterior) ? <FormattedMessage id='EXTERIOR' /> : ''}
-      </h2>
-      {color.storeStripLocator && (
-        <h3 className={`${BASE_CLASS}__rack-location`}>
-          <FormattedMessage id='LOCATION_NUMBER' />: {color.storeStripLocator}
-        </h3>
+      {shouldAllowFeature(config.featureExclusions, FEATURE_EXCLUSIONS.colorDetailsSubtitles) && (
+        <>
+          <h2 className={`${BASE_CLASS}__type`}>
+            {(color.isInterior) ? <FormattedMessage id='INTERIOR' /> : ''}
+            {(color.isInterior && color.isExterior) ? ' / ' : ''}
+            {(color.isExterior) ? <FormattedMessage id='EXTERIOR' /> : ''}
+          </h2>
+          {color.storeStripLocator && (
+            <h3 className={`${BASE_CLASS}__rack-location`}>
+              <FormattedMessage id='LOCATION_NUMBER' />: {color.storeStripLocator}
+            </h3>
+          )}
+        </>
       )}
     </div>
   )
 }
 
-export default ColorViewer
+export default WithConfigurationContext(ColorViewer)
