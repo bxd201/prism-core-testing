2e5b1b1b3c696ce4bc4eb33b9de537c70367174d
Fabio General <fabio.general.silva@sherwin.com>
2021-07-15 16:31:12 -0400

    CBGLCM-166 | Added layout for expert color picks when it has categories / tabs + color swatch option

--- src/components/ExpertColorPicks/ExpertColorPicks.jsx
+++ src/components/ExpertColorPicks/ExpertColorPicks.jsx
@@ -5,34 +5,47 @@ import CardMenu from 'src/components/CardMenu/CardMenu'
 import ExpertColorDetails from './ExpertColorDetails'
 import ColorStripButton from 'src/components/ColorStripButton/ColorStripButton'
 import Carousel from '../Carousel/Carousel'
+import ColorCollectionsTab from '../Shared/ColorCollectionsTab'
+import ColorSwatch from '../Facets/ColorWall/ColorSwatch/ColorSwatch'
 import { loadExpertColorPicks } from 'src/store/actions/expertColorPicks'
 import { fullColorNumber, getContrastYIQ } from 'src/shared/helpers/ColorUtils'
 import { useIntl } from 'react-intl'
 import at from 'lodash/at'
 import ConfigurationContext from 'src/contexts/ConfigurationContext/ConfigurationContext'
+import ColorWallContext from '../Facets/ColorWall/ColorWallContext'
 import './ExpertColorPicks.scss'
+import '../ColorStripButton/ColorStripButton.scss'
 
 const ExpertColorPicks = () => {
   const dispatch = useDispatch()
   const { locale } = useIntl()
   const { brandId } = useContext(ConfigurationContext)
-  React.useEffect(() => { loadExpertColorPicks(brandId, { language: locale })(dispatch) }, [])
+  useEffect(() => { loadExpertColorPicks(brandId, { language: locale })(dispatch) }, [])
 
   const { messages = {} } = useIntl()
   const [initPosition, setPosition] = useState(0)
+  const [tabId, setTabId] = useState('tab0')
   const expertColorPicks = useSelector(state => state.expertColorPicks.data)
+  const categories = expertColorPicks.map((ecp) => ecp.category?.[0]).filter((category, index, self) => category && self.indexOf(category) === index)
+  const expertColorPicksByCategory = expertColorPicks.filter((ecp) => ecp.category?.[0] === categories[tabId.slice(3)])
+  console.log(expertColorPicksByCategory)
+  const tabs = categories.map((category, index) => ({ id: `tab${index}`, tabName: category }))
+
   return (
     <CardMenu menuTitle={at(messages, 'EXPERT_COLOR_PICKS')[0]}>
       {(setCardShowing) => (
         <div className='expert-color-picks__wrapper'>
+          {categories.length > 0 && <ColorCollectionsTab collectionTabs={tabs} tabIdShow={tabId} showTab={setTabId} />}
           <div className='expert-color-picks__collections-list'>
             {expertColorPicks.length > 0 && <Carousel
-              BaseComponent={ColorStripButtonWrapper}
+              BaseComponent={expertColorPicks[0].colorDefs.length === 1 ? ColorSwatchWrapper : ColorStripButtonWrapper}
               btnRefList={[]}
               defaultItemsPerView={8}
               isInfinity={false}
               key='expertcolorpicks'
-              data={expertColorPicks}
+              data={expertColorPicksByCategory}
+              tabId={tabId}
+              setTabId={setTabId}
               setInitialPosition={setPosition}
               initPosition={initPosition}
               getSummaryData={collectionSummaryData => setCardShowing(<ExpertColorDetails expertColors={collectionSummaryData} />)}
@@ -78,6 +91,21 @@ const ColorStripButtonWrapper = (props: any) => {
         </div>
       </div>
     </ColorStripButton>
+  )
+}
+
+const ColorSwatchWrapper = ({ data }: any) => {
+  const color = data.colorDefs[0]
+
+  return (
+    <ColorWallContext.Provider value={{ displayAddButton: true, displayInfoButton: true }}>
+      <div className='color-strip-button'>
+        <div className='color-strip-button__swatch' role='button' tabIndex='0'>
+          <ColorSwatch style={{ position: 'absolute', height: '100px', width: '100%' }} color={color} showContents outline={false} />
+        </div>
+        <div className='color-strip-button__desc'>{data.description}</div>
+      </div>
+    </ColorWallContext.Provider>
   )
 }
 
