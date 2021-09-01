1da68281a432c7f09d8a155075d5051bd9864fab
Daniel Ryan <Daniel.S.Ryan@sherwin.com>
2021-09-01 08:55:31 -0500

    CBGLCM-2 PDF button

--- src/components/Shared/CollectionDetail.jsx
+++ src/components/Shared/CollectionDetail.jsx
@@ -36,11 +36,7 @@ const downloadPDF = (imagePath) => {
     responseType: 'blob'
   }).then((response) => {
     const url = window.URL.createObjectURL(new Blob([response.data]))
-    const link = document.createElement('a')
-    link.href = url
-    link.setAttribute('download', 'collection.pdf')
-    document.body.appendChild(link)
-    link.click()
+    window.open(url)
   })
 }
 
