a960c91a628eede90a701d1cd58a3366751c30c5
Brendan Do <brendan.do@sherwin.com>
2021-09-26 11:26:32 -0400

    feat: adding husky to run the linter validation and validate commit messages so it abides by commitlint's standards on commit

--- src/components/Facets/ColorWall/MinimalToolbar.jsx
+++ src/components/Facets/ColorWall/MinimalToolbar.jsx
@@ -1,19 +1,17 @@
 // @flow
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { Menu, MenuItem, Wrapper, Button } from 'react-aria-menubutton'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const MinimalToolbar = () => {
-//   const { sections = [], families = [], section: activeSection, family: activeFamily, primeColorWall } = useSelector(state => state.colors)
+  // const { sections = [], families = [], section: activeSection, family: activeFamily, primeColorWall } = useSelector(state => state.colors)
   const { families = [] } = useSelector(state => state.colors)
-  const [searchValue, setSearchValue] = useState()
 
-  console.log(families)
   return (
     <div style={{ display: 'flex', width: '100%', height: '50px' }}>
-      <input type='text' placeholder='Search for a color' onChange={e => setSearchValue(e.target.value)} />
+      <input type='text' placeholder='Search for a color' />
       {/* family selector */}
       <Wrapper onSelection={e => console.log(e)}>
         <Button style={{ height: '100%' }}>
