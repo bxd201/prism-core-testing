17c0167230c1f92ec762d747b88381388cffa600
Adam Wilson <adam.wilson1@sherwin.com>
2022-06-08 10:02:04 -0500

    fix: DCT-205 Add LRV sorting to color wall story

--- packages/toolkit/src/components/color-wall/color-wall.stories.tsx
+++ packages/toolkit/src/components/color-wall/color-wall.stories.tsx
@@ -15,10 +15,11 @@ import chunks from '../../test-utils/mocked-endpoints/chunks.json'
 const transpose = (matrix): string[][] => matrix[0].map((_, col) => matrix.map((row) => row[col]))
 
 const unChunkedChunksMap = (type): Color[][] => {
-  const chunkNames: any = type === 'family' ? sections.find(({ name }) => name === 'Sherwin-Williams Colors').families : sections
+  const chunkNames: any =
+    type === 'family' ? sections.find(({ name }) => name === 'Sherwin-Williams Colors').families : sections
 
   return chunkNames.reduce((map, value) => {
-    const getChunk = (el): any => type === 'family' ? el === 'name' ? value : [value] : value[el]
+    const getChunk = (el): any => (type === 'family' ? (el === 'name' ? value : [value]) : value[el])
 
     return {
       ...map,
@@ -37,23 +38,27 @@ const colorMap = colors.reduce((map, c) => {
   return map
 }, {})
 
-const order = { Descending: 'desc', Ascending: 'asc'}
+const order = { Descending: 'desc', Ascending: 'asc' }
 
 const Template = (args): JSX.Element => {
   const getColors = (map): Color[][] => {
-    const colors = map.map((chunk) => chunk.map(id => colorMap[id]))
+    const colors = map.map((chunk) => chunk.map((id) => colorMap[id]))
     return args.singleChunk ? [orderBy([...flatten(colors)], args.sortBy?.toLowerCase(), order[args.orderBy])] : colors
   }
 
   // Places hash on color hex when it doesn't have that
-  const hashesColorHex = (colors): Color[] => colors.map(color => ({ ...color, hex: `#${String(color.hex.replace(/#/g, ''))}` }))
+  const hashesColorHex = (colors): Color[] =>
+    colors.map((color) => ({ ...color, hex: `#${String(color.hex.replace(/#/g, ''))}` }))
 
   return (
     <ColorWall
       {...omit(args, 'family', 'orderBy', 'section', 'singleChunk', 'sortBy')}
-      colors={args.colors && args.colors.length > 0
-        ? [orderBy(hashesColorHex(args.colors), args.sortBy?.toLowerCase(), order[args.orderBy])]
-        : args.family ? getColors(unChunkedChunksMap('family')[args.family]) : getColors(unChunkedChunksMap('section')[args.section])
+      colors={
+        args.colors && args.colors.length > 0
+          ? [orderBy(hashesColorHex(args.colors), args.sortBy?.toLowerCase(), order[args.orderBy])]
+          : args.family
+          ? getColors(unChunkedChunksMap('family')[args.family])
+          : getColors(unChunkedChunksMap('section')[args.section])
       }
       zoomOutButtonRenderer={(onClick) => (
         <button
@@ -64,14 +69,17 @@ const Template = (args): JSX.Element => {
         </button>
       )}
       swatchRenderer={(defaultProps) => {
-        const { color }  = defaultProps
+        const { color } = defaultProps
 
         return (
           <ColorSwatch
             {...defaultProps}
             className='border-white border-1 ring-primary focus:outline-none focus:ring-2'
             renderer={() => (
-              <div className='absolute p-2' style={{ top: '-85%', left: '-85%', width: '270%', height: '270%', transform: 'scale(0.37)' }}>
+              <div
+                className='absolute p-2'
+                style={{ top: '-85%', left: '-85%', width: '270%', height: '270%', transform: 'scale(0.37)' }}
+              >
                 <div className='relative'>
                   <p className='text-sm'>{`${color.brandKey} ${color.colorNumber}`}</p>
                   <p className='font-bold'>{color.name}</p>
@@ -92,25 +100,70 @@ const Template = (args): JSX.Element => {
 }
 
 export const SherwinWilliamsColors = Template.bind({})
-SherwinWilliamsColors.args = { chunkWidth: 7, gridWidth: 8, singleChunk: false, section: 'Sherwin-Williams Colors', wrappingEnabled: false }
+SherwinWilliamsColors.args = {
+  chunkWidth: 7,
+  gridWidth: 8,
+  singleChunk: false,
+  section: 'Sherwin-Williams Colors',
+  wrappingEnabled: false
+}
 
 export const HistoricColors = Template.bind({})
-HistoricColors.args = { chunkHeight: 10, gridWidth: 2, singleChunk: false, section: 'Historic Colors', wrappingEnabled: true }
+HistoricColors.args = {
+  chunkHeight: 10,
+  gridWidth: 2,
+  singleChunk: false,
+  section: 'Historic Colors',
+  wrappingEnabled: true
+}
 
 export const EmeraldColors = Template.bind({})
-EmeraldColors.args = { chunkWidth: 5, gridWidth: 5, singleChunk: false, section: 'Emerald Designer Edition', wrappingEnabled: true }
+EmeraldColors.args = {
+  chunkWidth: 5,
+  gridWidth: 5,
+  singleChunk: false,
+  section: 'Emerald Designer Edition',
+  wrappingEnabled: true
+}
 
 export const LivingWellColors = Template.bind({})
-LivingWellColors.args = { chunkWidth: 4, gridWidth: 6, singleChunk: false, section: 'Living Well Colors', wrappingEnabled: true }
+LivingWellColors.args = {
+  chunkWidth: 4,
+  gridWidth: 6,
+  singleChunk: false,
+  section: 'Living Well Colors',
+  wrappingEnabled: true
+}
 
 export const SingleChunkEmeraldColors = Template.bind({})
-SingleChunkEmeraldColors.args = { chunkWidth: 20, gridWidth: 1, singleChunk: true, section: 'Emerald Designer Edition', wrappingEnabled: false }
+SingleChunkEmeraldColors.args = {
+  chunkWidth: 20,
+  gridWidth: 1,
+  singleChunk: true,
+  section: 'Emerald Designer Edition',
+  wrappingEnabled: false
+}
 
 export const SingleChunkRedColors = Template.bind({})
-SingleChunkRedColors.args = { chunkWidth: 20, gridWidth: 1, singleChunk: true, family: 'Red', section: 'Sherwin-Williams Colors', sortBy: 'Lightness', orderBy: 'Ascending', wrappingEnabled: false }
+SingleChunkRedColors.args = {
+  chunkWidth: 20,
+  gridWidth: 1,
+  singleChunk: true,
+  family: 'Red',
+  section: 'Sherwin-Williams Colors',
+  sortBy: 'Lightness',
+  orderBy: 'Ascending',
+  wrappingEnabled: false
+}
 
 export const WarmRedRedColors = Template.bind({})
-WarmRedRedColors.args = {  chunkWidth: 20, gridWidth: 1, sortBy: 'Lightness', orderBy: 'Ascending', colors: warmRedColors }
+WarmRedRedColors.args = {
+  chunkWidth: 20,
+  gridWidth: 1,
+  sortBy: 'Lightness',
+  orderBy: 'Ascending',
+  colors: warmRedColors
+}
 
 export default {
   title: 'ColorWall',
@@ -126,12 +179,13 @@ export default {
     },
     colors: {
       control: { type: 'array' },
-      description: 'adds json color data<br /><em>(overrides family and section when defined)</em>',
+      description: 'adds json color data<br /><em>(overrides family and section when defined)</em>'
     },
     family: {
       control: { type: 'select' },
-      description: 'defines the family to display colors from<br /><em>(overrides section when defined)</em><br /> `storybook args only`',
-      options: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Neutral', 'White & Pastel'],
+      description:
+        'defines the family to display colors from<br /><em>(overrides section when defined)</em><br /> `storybook args only`',
+      options: ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Neutral', 'White & Pastel']
     },
     gridWidth: {
       control: { type: 'range', min: 1, max: 20 },
@@ -154,7 +208,7 @@ export default {
     sortBy: {
       control: { type: 'select' },
       description: 'sorts colors by HSL<br /> `storybook args only`',
-      options: ['Hue', 'Saturation', 'Lightness'],
+      options: ['Hue', 'Saturation', 'Lightness', 'LRV']
     },
     swatchRenderer: { control: false },
     swatchSize: { table: { disable: true } },
