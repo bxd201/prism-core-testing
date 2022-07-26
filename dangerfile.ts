243f2f7179aec590f13dfd0388ad7838fb7ac347
Cody Richmond <cody.richmond@sherwin.com>
2022-07-26 11:46:35 -0400

    chore: DCT-NA: update dangerjs to not run on merges to qa or release
    Dangerjs shouldn't run on merges to higher environments in order to avoid false failures/warnings.

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,7 +2,17 @@ import { fail, warn, message, markdown, schedule, danger } from 'danger'
 import wordcheck from 'danger-plugin-wordcheck'
 import simpleCollection from 'danger-plugin-simple-collection'
 
+function shouldNotRunOnThisBaseBranch () {
+  const avoidBranches = ['qa', 'release']
+  if (avoidBranches.indexOf(danger.github.pr.base.ref) >= 0) {
+    return true 
+  }
+  return false
+}
+
 function validateCommitMessages () {
+  if (shouldNotRunOnThisBaseBranch()) return
+  
   danger.git.commits.forEach(commit => {
     if (!commit.message.match(/^(feat:)|(fix:)|(docs:)|(style:)|(test:)|(chore:)/g)) {
       warn(`Commit message '${commit.message}' does not match the correct format`)
@@ -11,12 +21,16 @@ function validateCommitMessages () {
 }
 
 function validatePullRequestTitle () {
+  if (shouldNotRunOnThisBaseBranch()) return
+  
   if (!danger.github.pr.title.match(/^DCT-\d* | '/g)) {
     warn('Pull request title does not match the format of `DCT-1234 | Message`')
   }
 }
 
 function ensurePackageChangesIncludeLockFile () {
+  if (shouldNotRunOnThisBaseBranch()) return
+  
   const packageChanged = danger.git.modified_files.includes('package.json')
   const lockfileChanged = danger.git.modified_files.includes('yarn.lock')
   if (packageChanged && !lockfileChanged) {
