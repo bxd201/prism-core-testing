41d52393aeb645e307a52e1e2d2ae573bad25643
Brendan Do <brendan.do@sherwin.com>
2022-08-23 09:18:47 -0400

    chore: Fixing DangerJS

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,36 +1,36 @@
-import { fail, warn, message, markdown, schedule, danger } from 'danger'
+import { warn, schedule, danger } from 'danger'
 import wordcheck from 'danger-plugin-wordcheck'
 import simpleCollection from 'danger-plugin-simple-collection'
 
-function shouldNotRunOnThisBaseBranch () {
+function shouldNotRunOnThisBaseBranch() {
   const avoidBranches = ['qa', 'release']
   if (avoidBranches.indexOf(danger.github.pr.base.ref) >= 0) {
-    return true 
+    return true
   }
   return false
 }
 
-function validateCommitMessages () {
+function validateCommitMessages() {
   if (shouldNotRunOnThisBaseBranch()) return
-  
-  danger.git.commits.forEach(commit => {
+
+  danger.git.commits.forEach((commit) => {
     if (!commit.message.match(/^(feat:)|(fix:)|(docs:)|(style:)|(test:)|(chore:)/g)) {
       warn(`Commit message '${commit.message}' does not match the correct format`)
     }
   })
 }
 
-function validatePullRequestTitle () {
+function validatePullRequestTitle() {
   if (shouldNotRunOnThisBaseBranch()) return
-  
+
   if (!danger.github.pr.title.match(/^[A-Z]+-\d* | '/g)) {
     warn('Pull request title does not match the format of `DCT-1234 | Message`')
   }
 }
 
-function ensurePackageChangesIncludeLockFile () {
+function ensurePackageChangesIncludeLockFile() {
   if (shouldNotRunOnThisBaseBranch()) return
-  
+
   const packageChanged = danger.git.modified_files.includes('package.json')
   const lockfileChanged = danger.git.modified_files.includes('yarn.lock')
   if (packageChanged && !lockfileChanged) {
@@ -50,7 +50,8 @@ simpleCollection({
   jiraUrl: 'https://sherwin-williams.atlassian.net/browse', // required, url to the account's/organization's JIRA home
   reportsPath: 'reports/danger', // optional default is reports/danger
   noConsoleWhitelist: ['warn', 'error', 'info'], // optional, whitelist options for console calls, possible options log,warn,info,error
-  disabled: { // optional, disable some plugins, options are listed in example below
+  disabled: {
+    // optional, disable some plugins, options are listed in example below
     coverage: true,
     jiraIssue: false,
     mentor: true,
