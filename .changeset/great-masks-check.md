af10e0b98a950b121138b0a0a3c43f63449860bd
Ron Sims II <ronald.sims@sherwin.com>
2022-09-13 13:47:27 -0400

    chore: Fixed upload button icon
    chore: Added changeset

---
+++ .changeset/great-masks-check.md
@@ -0,0 +1,5 @@
+---
+'@prism/facets': patch
+---
+
+Fixed bug that displayed download instead of upload icon [DCT-185]
--- packages/facets/src/components/Facets/RealColorFacet.jsx
+++ packages/facets/src/components/Facets/RealColorFacet.jsx
@@ -30,19 +30,20 @@ import RealColorView from '../RealColor/RealColorView'
 
 import 'src/providers/fontawesome/fontawesome'
 
-type RealColorFacetProps = FacetPubSubMethods & FacetBinderMethods & {
-  groupNames: string[],
-  maxSceneHeight: number,
-  defaultImage: string,
-  defaultMask: string,
-  forceSquare?: number,
-  defaultColor: string,
-  sceneName: string,
-  uploadButtonText: string,
-  waitMessage?: string
-}
+type RealColorFacetProps = FacetPubSubMethods &
+  FacetBinderMethods & {
+    groupNames: string[],
+    maxSceneHeight: number,
+    defaultImage: string,
+    defaultMask: string,
+    forceSquare?: number,
+    defaultColor: string,
+    sceneName: string,
+    uploadButtonText: string,
+    waitMessage?: string
+  }
 
-export function RealColorFacet (props: RealColorFacetProps) {
+export function RealColorFacet(props: RealColorFacetProps) {
   const { loadingConfiguration } = useContext(ConfigurationContext)
   // eslint-disable-next-line no-unused-vars
   const { groupNames, defaultMask, maxSceneHeight, sceneName, uploadButtonText, waitMessage } = props
@@ -52,7 +53,7 @@ export function RealColorFacet (props: RealColorFacetProps) {
   const [uploadInitiated, setUploadInitiated] = useState(false)
   const [uploadedImage, setUploadedImage] = useState(null)
   const [uploadedImageRefDims, setUploadedImageRefDims] = useState(null)
-  const activeColor = useSelector(store => store.lp.activeColor)
+  const activeColor = useSelector((store) => store.lp.activeColor)
   const [tintColor, setTintColor] = useState(null)
   const [colors] = useColors()
   // This flag is used to inform the facet if fastmask has been drawn to screen with api mask data
@@ -84,9 +85,11 @@ export function RealColorFacet (props: RealColorFacetProps) {
   const loadAndCrop = (payload: any, toHeight: number, makeSquare?: number) => {
     const { data: image, eventId } = payload
     const imageBlobUrl = URL.createObjectURL(image)
-    const imagePromise = makeSquare ? scaleAndCropImageToSquare(imageBlobUrl, makeSquare) : scaleImage(imageBlobUrl, toHeight)
+    const imagePromise = makeSquare
+      ? scaleAndCropImageToSquare(imageBlobUrl, makeSquare)
+      : scaleImage(imageBlobUrl, toHeight)
     imagePromise
-      .then(data => {
+      .then((data) => {
         setUploadedImage(data.url)
         setUploadedImageRefDims(extractRefDimensions(data))
         setFastMaskLoading(false)
@@ -94,10 +97,10 @@ export function RealColorFacet (props: RealColorFacetProps) {
         setUploadId(eventId)
         URL.revokeObjectURL(imageBlobUrl)
       })
-      .catch(err => setError(err))
+      .catch((err) => setError(err))
   }
 
-  const imageScaleCallback = data => {
+  const imageScaleCallback = (data) => {
     setInitialImageUrl(data.url)
     setInitialUploadedImageRefDims(extractRefDimensions(data))
     setTinterRendered(true)
@@ -108,11 +111,11 @@ export function RealColorFacet (props: RealColorFacetProps) {
     if (makeSquare) {
       scaleAndCropImageToSquare(imageUrl, makeSquare)
         .then(callback)
-        .catch(err => setError(err))
+        .catch((err) => setError(err))
     } else {
       scaleImage(imageUrl, toHeight)
         .then(callback)
-        .catch(err => setError(err))
+        .catch((err) => setError(err))
     }
   }
 
@@ -190,32 +193,61 @@ export function RealColorFacet (props: RealColorFacetProps) {
     setShouldShowInitialImage(true)
   }
 
-  return (<>
-    <div
-      className={!shouldShowInitialImage ? 'scene-visualizer--hidden' : 'scene-visualizer'}>
-      {fastMaskLoading ? <div className={'scene-visualizer__loader'}><BallSpinner /></div> : null}
-      {
-        !loadingConfiguration &&
-      initialImageUrl &&
-      initialUploadedImageRefDims &&
-      tintColor
-          ? <SceneVisualizerContent
+  return (
+    <>
+      <div className={!shouldShowInitialImage ? 'scene-visualizer--hidden' : 'scene-visualizer'}>
+        {fastMaskLoading ? (
+          <div className={'scene-visualizer__loader'}>
+            <BallSpinner />
+          </div>
+        ) : null}
+        {!loadingConfiguration && initialImageUrl && initialUploadedImageRefDims && tintColor ? (
+          <SceneVisualizerContent
             tinterRendered={tinterRendered}
             initUpload={initUpload}
             uploadInitiated={uploadInitiated}
             uploadButtonText={uploadButtonText}
-            tinter={defaultMask
-              ? <SimpleTintableScene
-              spinner={<BallSpinner />}
-              sceneType={SCENE_TYPES.ROOM}
-              sceneName={sceneName}
-              background={initialImageUrl}
-              surfaceUrls={[initialMaskImageUrl]}
-              surfaceIds={[facetId]}
-              surfaceColors={[tintColor]}
-              width={initialUploadedImageRefDims.imageWidth}
-              height={initialUploadedImageRefDims.imageHeight} />
-              : <RealColorView
+            tinter={
+              defaultMask ? (
+                <SimpleTintableScene
+                  spinner={<BallSpinner />}
+                  sceneType={SCENE_TYPES.ROOM}
+                  sceneName={sceneName}
+                  background={initialImageUrl}
+                  surfaceUrls={[initialMaskImageUrl]}
+                  surfaceIds={[facetId]}
+                  surfaceColors={[tintColor]}
+                  width={initialUploadedImageRefDims.imageWidth}
+                  height={initialUploadedImageRefDims.imageHeight}
+                />
+              ) : (
+                <RealColorView
+                  key={uploadId}
+                  activeColor={tintColor}
+                  spinner={<BallSpinner />}
+                  imageOpacity={0.5}
+                  waitMessage={waitMessage}
+                  handlerError={handleRealColorError}
+                  handleUpdate={handleRealColorUpdates}
+                  cleanupCallback={handleRealColorCleanup}
+                  imageUrl={initialImageUrl}
+                />
+              )
+            }
+          />
+        ) : null}
+      </div>
+      <div className={shouldShowInitialImage ? 'scene-visualizer--hidden' : 'scene-visualizer'}>
+        {uploadedImage && uploadedImageRefDims && tintColor && uploadId ? (
+          <SceneVisualizerContent
+            tinterRendered={tinterRendered}
+            handleFastMaskClose={handleRealColorClose}
+            initUpload={initUpload}
+            uploadInitiated={uploadInitiated}
+            uploadButtonText={uploadButtonText}
+            shouldShowCloseBtn
+            tinter={
+              <RealColorView
                 key={uploadId}
                 activeColor={tintColor}
                 spinner={<BallSpinner />}
@@ -224,31 +256,14 @@ export function RealColorFacet (props: RealColorFacetProps) {
                 handlerError={handleRealColorError}
                 handleUpdate={handleRealColorUpdates}
                 cleanupCallback={handleRealColorCleanup}
-                imageUrl={initialImageUrl} />} />
-          : null}</div>
-    <div className={shouldShowInitialImage ? 'scene-visualizer--hidden' : 'scene-visualizer'}>{
-      uploadedImage &&
-      uploadedImageRefDims &&
-      tintColor &&
-      uploadId
-        ? <SceneVisualizerContent
-          tinterRendered={tinterRendered}
-          handleFastMaskClose={handleRealColorClose}
-          initUpload={initUpload}
-          uploadInitiated={uploadInitiated}
-          shouldShowCloseBtn
-          tinter={<RealColorView
-            key={uploadId}
-            activeColor={tintColor}
-            spinner={<BallSpinner />}
-            imageOpacity={0.5}
-            waitMessage={waitMessage}
-            handlerError={handleRealColorError}
-            handleUpdate={handleRealColorUpdates}
-            cleanupCallback={handleRealColorCleanup}
-            imageUrl={uploadedImage} />} />
-        : null}</div>
-  </>)
+                imageUrl={uploadedImage}
+              />
+            }
+          />
+        ) : null}
+      </div>
+    </>
+  )
 }
 
 export default facetBinder(RealColorFacet, 'RealColorFacet')
