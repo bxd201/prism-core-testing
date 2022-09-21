6a9b7a32fb6c65da8ee15e5cd9b593f4235cde0e
fxg266 <fabio.general.silva@sherwin.com>
2022-09-21 15:58:06 -0400

    chore: [facets] DCT-430 | Created sw brazil and spanish templates

---
+++ .changeset/soft-cheetahs-rescue.md
@@ -0,0 +1,5 @@
+---
+'@prism/facets': minor
+---
+
+Added sherwin brazil and spanish color visualizer templates [DCT-430]
--- packages/facets/src/translations/translations.js
+++ packages/facets/src/translations/translations.js
@@ -1,17 +1,21 @@
 // import all supported languages
-import enUS from './en-US.json'
 import enCA from './en-CA.json'
-import frCA from './fr-CA.json'
+import enUS from './en-US.json'
 import esAR from './es-AR.json'
 import esEC from './es-EC.json'
+import esUS from './es-US.json'
+import frCA from './fr-CA.json'
+import ptBR from './pt-BR.json'
 
 // list all supported languages & associate with their JSON
 const languages = {
-  'en-US': enUS,
   'en-CA': enCA,
-  'fr-CA': frCA,
+  'en-US': enUS,
   'es-AR': esAR,
-  'es-EC': esEC
+  'es-EC': esEC,
+  'es-US': esUS,
+  'fr-CA': frCA,
+  'pt-BR': ptBR
 }
 
 export default languages
