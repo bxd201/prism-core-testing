9da498723c3eea96933b61bf0944a3f6535bb084
Cody Richmond <cody.richmond@sherwin.com>
2022-11-09 12:47:16 -0500

    chore: DCT-491: removing e2e tests from build
    E2E tests causing build to take 2+ hours to run, so removing for now.

--- ci/scripts/entrypoint.build.sh
+++ ci/scripts/entrypoint.build.sh
@@ -8,7 +8,8 @@ if [ -n "$DANGER_MANUAL_PR_NUM" ]; then
 fi
 
 NODE_ENV=test yarn test || exit $?
-yarn facets:e2e
+# commenting out for now because causing the build to take 2+ hours to run!!
+# yarn facets:e2e
 yarn run build || exit $?
 
 tar zcf dist.tgz packages/facets/dist packages/toolkit/dist packages/toolkit/public packages/prism-docs/build packages/prism-demo/build packages/toolkit/coverage/lcov-report
