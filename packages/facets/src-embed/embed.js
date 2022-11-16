b1ad622b3df2037cd19a0ab7122e081f64ba43e9
Cody Richmond <cody.richmond@sherwin.com>
2022-11-16 13:31:15 -0500

    chore: DCT-XX: increasing number of CSS artifacts generated during dev build
    ... in order to (potentially) mitigate issues arising from inclusion-order conflicts between styles.
    Also imposing more order around style loading based on importance.

--- packages/facets/src-embed/embed.js
+++ packages/facets/src-embed/embed.js
@@ -1,11 +1,11 @@
 // @flow
 import mapValues from 'lodash/mapValues'
-import { EMBED_ROOT_SELECTOR,EMBED_ROOT_SELECTOR_DEPRECATED } from 'src/facetSupport/facetConstants'
+import { EMBED_ROOT_SELECTOR, EMBED_ROOT_SELECTOR_DEPRECATED } from 'src/facetSupport/facetConstants'
 import embedScript from 'src/facetSupport/scripts/embedScript'
 import addHideStyles from 'src/facetSupport/styles/addHideStyles'
-import embedStyle from 'src/facetSupport/styles/embedStyle'
 import dressUpForPrism from 'src/facetSupport/utils/dressUpForPrism'
 import { updateGlobalPrismObject } from 'src/facetSupport/utils/globalPrismObject'
+import embedStyle from '../src/facetSupport/styles/embedStyle'
 
 const prismManifest = require('embedWorking/prismManifest.json') // eslint-disable-line
 
@@ -13,52 +13,56 @@ const promiseFor = {}
 const getPromiseFor = (facetName: string) => {
   if (promiseFor[facetName]) return promiseFor[facetName]
 
-  const scriptPromises = []
-  const targetedDependencies = prismManifest.filter(mod => {
-    // if GENERATE_FACET_ASSETS global is true, try to load provided facet by name
-    if (GENERATE_FACET_ASSETS) {
-      return mod.name === facetName
-    }
-    // otherwise just load the main bundle
-    return mod.main
-  }).map(mod => mod.dependencies).reduce((accum, next) => next, undefined)
+  const targetedDependencies = prismManifest
+    .filter((mod) => {
+      // if GENERATE_FACET_ASSETS global is true, try to load provided facet by name
+      if (GENERATE_FACET_ASSETS) {
+        return mod.name === facetName
+      }
+      // otherwise just load the main bundle
+      return mod.main
+    })
+    .map((mod) => mod.dependencies)
+    .reduce((accum, next) => next, undefined)
 
   if (!targetedDependencies) {
     throw new Error(`Invalid embed attempted for unknown Prism facet '${facetName}'.`)
   }
 
-  targetedDependencies.forEach(dep => {
-    if (dep.match(/\.css$/)) {
-      embedStyle(dep)
-    } else if (dep.match(/\.js$/)) {
-      scriptPromises.push(embedScript(dep, facetName))
-    }
-  })
+  // embed our styles
+  embedTheStyles(targetedDependencies.filter((dep) => dep.match(/\.css$/)))
 
-  promiseFor[facetName] = Promise.all(scriptPromises)
-    .then(() => new Promise((resolve, reject) => {
-      const timeout = Date.now() + (1000 * 10) // 10s timeout to locate embed method
-      const checkForEmbedMethod = () => {
-        console.info(`Looking for embed method for ${facetName}`)
-        const method = window.PRISM && window.PRISM.facets && window.PRISM.facets[facetName] // look for internal embed method created by bundle.js
-        if (typeof method === 'function') {
-          return resolve(method)
-        }
-        if (Date.now() > timeout) {
-          return reject(new Error(`Prism embed method not found for facet '${facetName}'.`))
-        }
-        setTimeout(checkForEmbedMethod, 100)
-      }
+  // aggregate all script embed promises
+  const scriptPromises = targetedDependencies
+    .filter((dep) => dep.match(/\.js$/))
+    .map((dep) => embedScript(dep, facetName))
 
-      checkForEmbedMethod()
-    }))
+  promiseFor[facetName] = Promise.all(scriptPromises).then(
+    () =>
+      new Promise((resolve, reject) => {
+        const timeout = Date.now() + 1000 * 10 // 10s timeout to locate embed method
+        const checkForEmbedMethod = () => {
+          console.info(`Looking for embed method for ${facetName}`)
+          const method = window.PRISM && window.PRISM.facets && window.PRISM.facets[facetName] // look for internal embed method created by bundle.js
+          if (typeof method === 'function') {
+            return resolve(method)
+          }
+          if (Date.now() > timeout) {
+            return reject(new Error(`Prism embed method not found for facet '${facetName}'.`))
+          }
+          setTimeout(checkForEmbedMethod, 100)
+        }
+
+        checkForEmbedMethod()
+      })
+  )
 
   return promiseFor[facetName]
 }
 
 // eslint-disable-next-line
 const prismPromise = new Promise((resolvePrism, rejectPrism) => {
-  const embedQueue = new function () {
+  const embedQueue = new (function () {
     const embedMethodFor = {}
     const embedRequestsFor = {}
 
@@ -71,7 +75,9 @@ const prismPromise = new Promise((resolvePrism, rejectPrism) => {
 
       // targeted Prism Facet should be identified by either dataset.prismFacet or props.prismFacet
       // if not, I guess we can drop this particular request?
-      const attrProps: {[key: string]: string} = mapValues(Object.assign({}, el.dataset), v => v === '' ? true : v)
+      const attrProps: { [key: string]: string } = mapValues(Object.assign({}, el.dataset), (v) =>
+        v === '' ? true : v
+      )
       const { reactComponent, prismFacet } = attrProps
       // eslint-disable-next-line
       const chosenFacet: string | typeof undefined = props.prismFacet || prismFacet || reactComponent
@@ -95,7 +101,7 @@ const prismPromise = new Promise((resolvePrism, rejectPrism) => {
       embedRequestsFor[chosenFacet].push(args)
 
       getPromiseFor(chosenFacet)
-        .then(embedMethod => {
+        .then((embedMethod) => {
           if (!embedMethod) {
             throw new Error(`No embed method found for ${chosenFacet}`)
           }
@@ -103,28 +109,38 @@ const prismPromise = new Promise((resolvePrism, rejectPrism) => {
           embedMethodFor[chosenFacet] = embedMethod
           embedMethod.apply(undefined, args)
         })
-        .catch(err => {
+        .catch((err) => {
           console.info(`Error getting promise for, or embedding, ${chosenFacet}.`)
           console.error(err)
         })
     }
 
     return this
-  }()
+  })()
 
   updateGlobalPrismObject('embed', embedQueue.add)
-  updateGlobalPrismObject('availableFacets', prismManifest.map(({  name  }) => name))
+  updateGlobalPrismObject(
+    'availableFacets',
+    prismManifest.map(({ name }) => name)
+  )
 
   addHideStyles()
 
   const prismRootLegacy = Array.from(document.querySelectorAll(EMBED_ROOT_SELECTOR_DEPRECATED))
   const prismRootModern = Array.from(document.querySelectorAll(EMBED_ROOT_SELECTOR))
-  const allRoots = [
-    ...prismRootLegacy,
-    ...prismRootModern
-  ]
+  const allRoots = [...prismRootLegacy, ...prismRootModern]
 
   if (allRoots.length > 0) {
-    allRoots.map(el => dressUpForPrism(el)).map(el => embedQueue.add(el))
+    allRoots.map((el) => dressUpForPrism(el)).map((el) => embedQueue.add(el))
   }
 })
+
+function embedTheStyles(stylesheets) {
+  const thisStylesheet = stylesheets.shift()
+  if (thisStylesheet) {
+    setTimeout(() => {
+      embedStyle(thisStylesheet)
+      embedTheStyles(stylesheets)
+    }, 10)
+  }
+}
