7f78a3f6df795d7c37b670dd61fbd0c0de50b028
Sam Thompson <sam.thompson@sherwin.com>
2022-12-08 16:07:20 -0600

    feat: Add root color variables and docs
    Updates tailwind config to use new variables
    Improves storybook/tailwind integration

--- packages/toolkit/.storybook/main.js
+++ packages/toolkit/.storybook/main.js
@@ -1,15 +1,23 @@
+const path = require('path')
+
 module.exports = {
-  stories: ['../src/components/**/*.stories.tsx'],
+  stories: ['../src/components/**/*.stories.@(tsx|mdx)', '../src/stories/**/*.stories.@(tsx|mdx)'],
   // Add any Storybook addons you want here: https://storybook.js.org/addons/
   addons: ['@storybook/addon-essentials', './addons/testTab/register', '@storybook/preset-create-react-app'],
+  staticDirs: ['../src/styles'],
   webpackFinal: async (config) => {
     config.module.rules.push({
-      test: /\.css$/,
+      test: /\,css&/,
       use: [
         {
-          loader: 'postcss-loader'
+          loader: 'postcss-loader',
+          options: {
+            ident: 'postcss',
+            plugins: [require('tailwindcss'), require('autoprefixer')]
+          }
         }
-      ]
+      ],
+      include: path.resolve(__dirname, '../')
     })
     return config
   },
