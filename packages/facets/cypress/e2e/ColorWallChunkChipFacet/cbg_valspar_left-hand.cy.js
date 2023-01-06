f13d595db1b58402ab3e3722f3bee19549510731
fxg266 <fabio.general.silva@sherwin.com>
2023-01-06 14:17:11 -0500

    test: DCT-674 | Refactored e2e tests

--- packages/facets/cypress/e2e/ColorWallChunkChipFacet/cbg_valspar_left-hand.cy.js
+++ packages/facets/cypress/e2e/ColorWallChunkChipFacet/cbg_valspar_left-hand.cy.js
@@ -1,15 +1,18 @@
-describe('valspar-qr-color-wall-left-hand', () => {
-  before(() => cy.visit('cbg/valspar-qr-color-wall-left-hand.html'))
+describe('valspar-qr-color-wall-cwv3-left-hand', () => {
+  context('Color Wall', () => {
+    before(() => cy.visit('cbg/valspar-qr-color-wall-cwv3-left-hand.html'))
 
-  it('displays color wall', () => {
-    cy.get('.cw2__wall').as('wall')
-    cy.wait(Cypress.env('wait_time')) // waiting for rendering
-    cy.get('@wall').snapshot({ name: 'wall' })
-  })
+    it('displays color wall', () => {
+      cy.findByTestId('wall-height-div').as('wall')
+      cy.wait(Cypress.env('wait_time')) // waiting for rendering
+      cy.get('@wall').snapshot({ name: 'wall' })
+    })
 
-  it('clicks on a swatch and displays correct content', () => {
-    cy.get('.cw2__chunk--clickable').first().click()
-    cy.findByLabelText('4001-1A White Pepper').click().snapshot({ name: 'swatch' })
-    cy.get('.color-swatch__content').snapshot({ name: 'swatch content' })
+    it('clicks on a chunk and on a swatch and displays correct content', () => {
+      cy.findAllByTestId('wall-chunk').first().click()
+      cy.findByTestId('wall-color-swatch-1345').click()
+      .snapshot({ name: 'swatch' })
+      cy.findByTestId('inner-swatch-1345').snapshot({ name: 'swatch content' })
+    })
   })
 })
