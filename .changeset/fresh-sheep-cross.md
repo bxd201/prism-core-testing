b9668a5ec61320d75fd966d015fe1e06b5d4a595
fxg266 <fabio.general.silva@sherwin.com>
2023-01-13 10:55:52 -0500

    chore: MESP-6600 | Removed header texts and cleanup on ColorWallChunkChipFacet

---
+++ .changeset/fresh-sheep-cross.md
@@ -0,0 +1,5 @@
+---
+'@prism/facets': patch
+---
+
+Removed header texts and cleanup on ColorWallChunkChipFacet [MESP-6600]
--- packages/facets/src/components/Facets/ColorWallChunkChipFacet/ColorWallChunkChipFacet.jsx
+++ packages/facets/src/components/Facets/ColorWallChunkChipFacet/ColorWallChunkChipFacet.jsx
@@ -1,17 +1,12 @@
 // @flow
-import React, { useContext, useEffect,useMemo } from 'react'
-import { useIntl } from 'react-intl'
-import { useDispatch } from 'react-redux'
+import React, { useMemo } from 'react'
 import ColorWallContext, { colorWallContextDefault } from 'src/components/Facets/ColorWall/ColorWallContext'
-import ConfigurationContext from 'src/contexts/ConfigurationContext/ConfigurationContext'
 import facetBinder from 'src/facetSupport/facetBinder'
 import extendIfDefined from 'src/shared/helpers/extendIfDefined'
 import type { Color } from 'src/shared/types/Colors.js.flow'
 import translateBooleanFlexibly from 'src/shared/utils/translateBooleanFlexibly.util'
-import { loadColors } from 'src/store/actions/loadColors'
 import ColorWallRouter from '../ColorWall/ColorWallRouter'
 import ColorWallV3 from '../ColorWall/ColorWallV3'
-import '../ColorWall/ColorWall.scss'
 
 type Props = {
   autoHeight?: boolean,
@@ -37,39 +32,36 @@ export const ColorWallChunkChipFacet = (props: Props) => {
     leftHandDisplay,
     wallBanner
   } = props
-  const cwContext = useMemo(() => extendIfDefined({}, colorWallContextDefault, {
-    autoHeight: translateBooleanFlexibly(autoHeight),
-    chunkClickable: translateBooleanFlexibly(chunkClickable),
-    chunkMiniMap: translateBooleanFlexibly(chunkMiniMap),
-    colorDetailPageRoot,
-    colorWallBgColor,
-    colorWallPageRoot,
-    displayDetailsLink: translateBooleanFlexibly(displayDetailsLink),
-    isChipLocator: true,
-    leftHandDisplay: translateBooleanFlexibly(leftHandDisplay)
-  }), [autoHeight, chunkClickable, chunkMiniMap, colorDetailPageRoot, colorWallBgColor, colorWallPageRoot, displayDetailsLink, leftHandDisplay])
-
-  const { brandId } = useContext(ConfigurationContext)
-  const dispatch = useDispatch()
-  const { locale } = useIntl()
-
-  useEffect(() => {
-    if (!locale || !brandId) return
-    // @todo we could refactor so that comps rely on useColor hook -RS
-    dispatch(loadColors(brandId, { language: locale }))
-  }, [locale, brandId])
+  const cwContext = useMemo(
+    () =>
+      extendIfDefined({}, colorWallContextDefault, {
+        autoHeight: translateBooleanFlexibly(autoHeight),
+        chunkClickable: translateBooleanFlexibly(chunkClickable),
+        chunkMiniMap: translateBooleanFlexibly(chunkMiniMap),
+        colorDetailPageRoot,
+        colorWallBgColor,
+        colorWallPageRoot,
+        displayDetailsLink: translateBooleanFlexibly(displayDetailsLink),
+        isChipLocator: true,
+        leftHandDisplay: translateBooleanFlexibly(leftHandDisplay)
+      }),
+    [
+      autoHeight,
+      chunkClickable,
+      chunkMiniMap,
+      colorDetailPageRoot,
+      colorWallBgColor,
+      colorWallPageRoot,
+      displayDetailsLink,
+      leftHandDisplay
+    ]
+  )
 
   return (
     <ColorWallContext.Provider value={cwContext}>
       <ColorWallRouter defaultSection={undefined}>
-        <div style={{ margin: '0 1em' }}>
-          <h1 className='color-wall__title'>Find Chip.</h1>
-          <h2 className='color-wall__subtitle'>Click on a section to view colors</h2>
-          {wallBanner
-            ? <img src={wallBanner} style={{ maxWidth: '100%', margin: '0 auto .5em' }} />
-            : null }
-          <ColorWallV3 />
-        </div>
+        {wallBanner && <img src={wallBanner} style={{ maxWidth: '100%', margin: '0 auto .5em' }} />}
+        <ColorWallV3 />
       </ColorWallRouter>
     </ColorWallContext.Provider>
   )
